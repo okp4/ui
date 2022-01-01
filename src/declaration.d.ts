092f4779d2541c6d995ff6189ed0835b5f29796c
Christophe Camel <camel.christophe@gmail.com>
2022-01-01 14:32:07 +0100

    chore(project): add more module types (raw-loader, png, jpg)

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -2,3 +2,17 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string }
   export default classes
 }
+
+declare module '!!raw-loader!*' {
+  const contents: string
+  export = contents
+}
+declare module '*.png' {
+  const value: any
+  export = value
+}
+
+declare module '*.jpg' {
+  const value: any
+  export = value
+}
