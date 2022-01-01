791fd5b809d1720df50550e704e3a3faecd5133c
Christophe Camel <camel.christophe@gmail.com>
2022-01-01 14:31:23 +0100

    chore(project): configure webpack to support glsl resources

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,22 +14,26 @@ module.exports = {
       path.resolve('./src'),
     ]
 
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: [/\.scss$/, /\.module.scss$/],
-      // use: ['sass-loader'],
-      loader: 'sass-loader',
-      options: {
-        sassOptions: {
-          // indentWidth: 4,
-          log: console.log(
-            '\n\n\n\npath.resolve(__dirname, "src/sass/")',
-            path.resolve(__dirname, '../src/')
-          ),
-          includePaths: [path.resolve(__dirname, 'src/')],
+    config.module.rules.push(
+      {
+        test: [/\.scss$/, /\.module.scss$/],
+        loader: 'sass-loader',
+        options: {
+          sassOptions: {
+            // indentWidth: 4,
+            log: console.log(
+              '\n\n\n\npath.resolve(__dirname, "src/sass/")',
+              path.resolve(__dirname, '../src/')
+            ),
+            includePaths: [path.resolve(__dirname, 'src/')],
+          },
         },
       },
-    })
+      {
+        test: [/\.frag$/, /\.vert$/],
+        use: 'raw-loader',
+      }
+    )
 
     return config
   },
