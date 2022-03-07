55b94f9f1f858cff4c427c4863f9bc30981f2bb5
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-03-07 12:17:03 +0100

    feat(storybook): implement color components and utils consumed by palette story

---
+++ .storybook/components/ColorSet.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StorybookColor } from './Color'
+import { ColorKey } from './utils'
+
+import './index.scss'
+
+export type StorybookColorSetProps = {
+    /**
+     * References the keys of exported the variable in the color palette.
+     */
+    colorKeys: Array<ColorKey>
+}
+
+export const StorybookColorSet: React.FC<StorybookColorSetProps> = ({
+    colorKeys
+}: Readonly<StorybookColorSetProps>) => (
+    <ul className="storybook color-set">
+        {colorKeys.map((colorKey: ColorKey) => {
+            return <StorybookColor key={colorKey} colorKey={colorKey}/>
+        })}
+    </ul>
+)
