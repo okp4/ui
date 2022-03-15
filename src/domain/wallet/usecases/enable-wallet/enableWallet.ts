e742896ec442ede57dee4390f0bc039ed1f831fc
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-03-15 18:28:55 +0100

    feat(core): add account dispatch in wallet usecase

--- src/domain/wallet/usecases/enable-wallet/enableWallet.ts
+++ src/domain/wallet/usecases/enable-wallet/enableWallet.ts
@@ -1,6 +1,9 @@
-import { ConnectionError } from 'domain/wallet/entities/errors'
-import { ChainId } from 'domain/wallet/entities/wallet'
-import { ThunkResult } from '../../store/store'
+import {
+  ConnectionError,
+  UnspecifiedError,
+} from 'domain/wallet/entities/errors'
+import { Accounts, ChainId } from 'domain/wallet/entities/wallet'
+import { ReduxStore, ThunkResult } from '../../store/store'
 import { ErrorWalletActions } from '../actionCreators'
 import { EnableWalletActions } from './actionCreators'
 
@@ -8,20 +11,39 @@ export const enableWallet =
   (chainId: ChainId): ThunkResult<Promise<void>> =>
   async (dispatch, getState, { walletGateway }) => {
     try {
-      const result = await walletGateway.enable(chainId)
+      const result = await walletGateway.connect(chainId)
       if (isError(result)) {
-        dispatch(ErrorWalletActions.walletFailed(result))
+        dispatchError(result, dispatch)
       } else {
-        dispatch(EnableWalletActions.walletEnabled(chainId))
+        dispatchConnectionStatuses(chainId, dispatch)
+        const accounts = await walletGateway.getAccounts(chainId)
+        dispatchAccounts(accounts, chainId, dispatch)
       }
     } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'unknwon error'
-      dispatch(
-        ErrorWalletActions.walletFailed(new ConnectionError(errorMessage))
-      )
+      dispatchError(error, dispatch)
     }
   }
 
 const isError = (result: ConnectionError | void): result is ConnectionError =>
   result instanceof ConnectionError
+
+const dispatchAccounts = (
+  accounts: Accounts,
+  chainId: ChainId,
+  dispatch: ReduxStore['dispatch']
+) => {
+  dispatch(EnableWalletActions.accountsRetrieved(chainId, accounts))
+}
+
+const dispatchError = (error: unknown, dispatch: ReduxStore['dispatch']) => {
+  const errorToDispatch =
+    error instanceof Error ? new ConnectionError() : new UnspecifiedError()
+  dispatch(ErrorWalletActions.walletFailed(errorToDispatch))
+}
+
+const dispatchConnectionStatuses = (
+  chainId: ChainId,
+  dispatch: ReduxStore['dispatch']
+) => {
+  dispatch(EnableWalletActions.walletConnected(chainId))
+}
