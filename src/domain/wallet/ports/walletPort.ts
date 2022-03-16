af09812682bd30a3bd2f641ee7eb87fd4626497e
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-03-16 15:07:45 +0100

    feat(core): declare WalletRegistryPort main port

--- src/domain/wallet/ports/walletPort.ts
+++ src/domain/wallet/ports/walletPort.ts
@@ -1,8 +1,16 @@
 import { ConnectionError } from '../entities/errors'
 import { Accounts, ChainId } from '../entities/wallet'
 
-export type WalletPort = {
+export type WalletRegistryPort = {
+  get: (id: WalletId) => Wallet
+  names: () => string[]
+}
+
+export type Wallet = {
+  id: () => WalletId
   connect: (chainId: ChainId) => Promise<void | ConnectionError>
   isConnected: () => boolean
   getAccounts: (chainId: ChainId) => Promise<Accounts>
 }
+
+export type WalletId = string
