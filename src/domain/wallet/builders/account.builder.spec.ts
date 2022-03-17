8883b8f2c646fa651833f9d4a8dcf21ec912a6cf
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-03-17 13:18:43 +0100

    feat(core): add invariants for builder + builder test

---
+++ src/domain/wallet/builders/account.builder.spec.ts
@@ -0,0 +1,53 @@
+import { UnspecifiedError } from '../entities/errors'
+import { AccountBuilder } from './account.builder'
+
+describe('Build an Account', () => {
+  const buildAccount = (adress: string, pubKey: Uint8Array) =>
+    new AccountBuilder().withAddress(adress).withPublicKey(pubKey).build()
+
+  describe('Given that address is empty', () => {
+    const address = ''
+    const pubKey = new Uint8Array(2)
+    describe('When building account', () => {
+      const account = () => buildAccount(address, pubKey)
+      test('Then, expect builder to throw an UnspecifiedError ', () => {
+        expect(account).toThrowError(UnspecifiedError)
+      })
+    })
+  })
+
+  describe('Given that publicKey is empty', () => {
+    const address = 'address1'
+    const pubKey = new Uint8Array(0)
+    describe('When building account', () => {
+      const account = () => buildAccount(address, pubKey)
+      test('Then, expect builder to throw an UnspecifiedError ', () => {
+        expect(account).toThrowError(UnspecifiedError)
+      })
+    })
+  })
+
+  describe('Given that no adress and no publicKey are provided', () => {
+    describe('When building account', () => {
+      const account = () => new AccountBuilder().build()
+      test('Then, expect builder to throw an UnspecifiedError ', () => {
+        expect(account).toThrowError(UnspecifiedError)
+      })
+    })
+  })
+
+  describe('Given that address and publicKey are correctly provided', () => {
+    const address = 'address1'
+    const pubKey = new Uint8Array(2)
+    describe('When building account', () => {
+      const account = buildAccount(address, pubKey)
+      test('Then, expect builder to construct a valid Account ', () => {
+        expect(account).toEqual({
+          address: 'address1',
+          algorithm: 'secp256k1',
+          publicKey: new Uint8Array(2),
+        })
+      })
+    })
+  })
+})
--- src/domain/wallet/builders/account.builder.ts
+++ src/domain/wallet/builders/account.builder.ts
@@ -1,4 +1,5 @@
-import { Account } from 'domain/wallet/entities/wallet'
+import { Account, Algorithm } from 'domain/wallet/entities/wallet'
+import { UnspecifiedError } from '../entities/errors'
 
 export class AccountBuilder {
   private readonly account: Account
@@ -6,21 +7,40 @@ export class AccountBuilder {
   constructor() {
     this.account = {
       address: '',
+      algorithm: 'secp256k1',
       publicKey: new Uint8Array(),
     }
   }
 
   public withAddress(address: string): AccountBuilder {
+    if (!address.length) {
+      throw new UnspecifiedError()
+    }
     this.account.address = address
     return this
   }
 
+  public withAlgorithm(algorithm: Algorithm): AccountBuilder {
+    this.account.algorithm = algorithm
+    return this
+  }
+
   public withPublicKey(key: Uint8Array): AccountBuilder {
+    if (!key.length) {
+      throw new UnspecifiedError()
+    }
     this.account.publicKey = key
     return this
   }
 
   public build(): Account {
+    if (!this.invariant()) {
+      throw new UnspecifiedError()
+    }
     return this.account
+  }
+
+  private invariant(): boolean {
+    return this.account.address.length > 0 && this.account.publicKey.length > 0
   }
 }
