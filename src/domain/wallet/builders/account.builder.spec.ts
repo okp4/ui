6376a3297a44ec25d5d3b2a7dacffd9bbd2335fb
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-03-18 10:43:40 +0100

    test: add algorithm building in account.builder specification

--- src/domain/wallet/builders/account.builder.spec.ts
+++ src/domain/wallet/builders/account.builder.spec.ts
@@ -1,15 +1,25 @@
 import { UnspecifiedError } from '../entities/errors'
+import { Algorithm } from '../entities/wallet'
 import { AccountBuilder } from './account.builder'
 
 describe('Build an Account', () => {
-  const buildAccount = (adress: string, pubKey: Uint8Array) =>
-    new AccountBuilder().withAddress(adress).withPublicKey(pubKey).build()
+  const buildAccount = (
+    adress: string,
+    pubKey: Uint8Array,
+    algorithm: Algorithm
+  ) =>
+    new AccountBuilder()
+      .withAddress(adress)
+      .withPublicKey(pubKey)
+      .withAlgorithm(algorithm)
+      .build()
 
   describe('Given that address is empty', () => {
     const address = ''
     const pubKey = new Uint8Array(1)
+    const algorithm: Algorithm = 'sr25519'
     describe('When building account', () => {
-      const account = () => buildAccount(address, pubKey)
+      const account = () => buildAccount(address, pubKey, algorithm)
       test('Then, expect builder to throw an UnspecifiedError ', () => {
         expect(account).toThrowError(UnspecifiedError)
       })
@@ -19,8 +29,9 @@ describe('Build an Account', () => {
   describe('Given that publicKey is empty', () => {
     const address = 'address1'
     const pubKey = new Uint8Array(0)
+    const algorithm: Algorithm = 'sr25519'
     describe('When building account', () => {
-      const account = () => buildAccount(address, pubKey)
+      const account = () => buildAccount(address, pubKey, algorithm)
       test('Then, expect builder to throw an UnspecifiedError ', () => {
         expect(account).toThrowError(UnspecifiedError)
       })
@@ -36,15 +47,16 @@ describe('Build an Account', () => {
     })
   })
 
-  describe('Given that address and publicKey are correctly provided', () => {
+  describe('Given that address, publicKey and algorithm are correctly provided', () => {
     const address = 'address2'
     const pubKey = new Uint8Array(2)
+    const algorithm: Algorithm = 'ed25519'
     describe('When building account', () => {
-      const account = buildAccount(address, pubKey)
+      const account = buildAccount(address, pubKey, algorithm)
       test('Then, expect builder to construct a valid Account ', () => {
         expect(account).toEqual({
           address: 'address2',
-          algorithm: 'secp256k1',
+          algorithm: 'ed25519',
           publicKey: new Uint8Array(2),
         })
       })
