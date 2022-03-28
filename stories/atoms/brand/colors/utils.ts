7d7eff293070f52c2a4c6b5641913e3ddd5670d8
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-03-28 16:49:51 +0200

    feat(stories): move folders + implement new colors stories

--- stories/atoms/brand/colors/utils.ts
+++ stories/atoms/brand/colors/utils.ts
@@ -7,9 +7,9 @@ export type ColorKey = string
  * @returns the variable name of the color (in saas referential).
  */
 export const colorVariable = (colorKey: string) => {
-    const [_, ...tail] = colorKey.split('-')
+  const [theme, _set, ...tail] = colorKey.split('-')
 
-    return `$color-${tail.join('-')}`
+  return `$color-${theme}-${tail.join('-')}`
 }
 
 /**
@@ -17,14 +17,13 @@ export const colorVariable = (colorKey: string) => {
  * @returns the name of the color (human readable derived from the key).
  */
 export const colorName = (colorKey: string) => {
-    const [_, ...tail] = colorKey.split('-')
-
-    return `${tail.join(' ').toLowerCase()}`
+  const [_theme, _set, ...tail] = colorKey.split('-')
+  return `${tail.join(' ').toLowerCase()}`
 }
 
 /**
  * @param set the set from which the colors are retrieved.
  * @returns all the color keys which belong to the provided set.
  */
-export const colorsSet = (set: string) =>
-    Object.keys(colors).filter(color => color.indexOf(set + '-') === 0)
+export const colorsSet = (theme: string, set: string) =>
+  Object.keys(colors).filter(color => color.indexOf(`${theme}-${set}-`) === 0)
