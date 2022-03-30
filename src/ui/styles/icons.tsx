b1ff9422f56296c0522d14d6b4b5138ad3f1f6c8
Adrien <a.touzouli@okp4.com>
2022-03-30 14:59:04 +0200

    redesign: an better way of importing icons

--- src/ui/styles/icons.tsx
+++ src/ui/styles/icons.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 
-export type IconEnum = 'ActiveMenuLink' | 'Add' | 'Alerts' | 'Chat' | 'DownArrow' | 'Meetup' | 'Next' | 'Previous' | 'Search' | 'UpArrow'; 
+export type IconEnum = 'ActiveMenuLink' | 'Add' | 'Alerts' | 'Chat' | 'DownArrow' | 'Meetup' | 'Next' | 'Previous' | 'Search' | 'UpArrow';
 
 export type TIconProps = Readonly<{
   readonly iconName?: IconEnum,
@@ -8,29 +8,53 @@ export type TIconProps = Readonly<{
   readonly className?: string,
 }>
 
+type IconEntry = Readonly<{
+  iconName: IconEnum,
+  src: string
+}>
+
+type IconMap = {
+  light: IconEntry[],
+  dark: IconEntry[],
+}
+
+const iconMap: IconMap = {
+  light: [],
+  dark: [],
+}
+
+/**
+ * Load icons
+ */
+const ligthCtx = require.context('/public/icons/light', false, /\.(svg)$/);
+const darkCtx = require.context('/public/icons/dark', false, /\.(svg)$/);
+function importThemeIcons(theme: 'light' | 'dark'): void {
+  const ctx = theme === 'light' ? ligthCtx : darkCtx;
+
+  ctx.keys().map((key: string) => {
+    const iconName = key.replace('./', '').replace('.svg', '').replace('Icone_', '') as IconEnum;
+    const icon = ctx(key);
+    iconMap[theme].push({ iconName, src: icon });
+  });
+}
+
+importThemeIcons('light');
+importThemeIcons('dark');
+
+function getIcon(iconName: IconEnum, theme: 'light' | 'dark'): string | undefined {
+
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+  return iconMap[theme].find((x: IconEntry) => x.iconName === iconName)?.src;
+}
+
 export const Icon: React.FC<TIconProps> = ({
   iconName = 'Meetup',
   theme = 'light',
   className,
 }: TIconProps): JSX.Element => {
-
-  const defaultImage = ''
-  // eslint-disable-next-line @typescript-eslint/typedef
-  const [image, setImage] = useState<string>('')
-
-  useEffect(() => {
-    const fetchImage = async (): Promise<void> => {
-      try {
-        const response = await import(`/public/icons/${theme}/Icone_${iconName}.svg`)
-        setImage(response.default)
-      } catch (err: unknown) {
-        const response = await import(defaultImage)
-        setImage(response.default)
-      }
-    }
-
-    fetchImage();
-  }, [iconName, theme]);
-
-  return <img alt={`${iconName} icon`} className={className} src={image} />
+  const iconSrc = getIcon(iconName, theme);
+  if (!iconSrc) {
+    throw new Error(`Icon ${iconName} not found`);
+  }
+  return <img alt={`${iconName} icon`} className={className} src={iconSrc} />
 }
