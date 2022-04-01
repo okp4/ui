4c08db5db2437a29354080fd6d84c041e020b763
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-01 22:35:14 +0200

    feat(component): load last saved theme at mount

--- src/ui/atoms/theme/ThemeSwitcher.tsx
+++ src/ui/atoms/theme/ThemeSwitcher.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import * as Switch from '@radix-ui/react-switch'
 import MoonIcon from '../../../assets/icons/moonIcon.svg'
 import SunIcon from '../../../assets/icons/sunIcon.svg'
-import type { ThemeContextType } from './themeContext'
+import type { Theme, ThemeContextType } from './themeContext'
 import { useTheme } from 'hooks/useTheme'
 import './themeSwitcher.scss'
 
@@ -10,16 +10,31 @@ export const ThemeSwitcher: React.FC = () => {
   const { theme, setTheme }: ThemeContextType = useTheme()
   const isDarkTheme = theme === 'dark'
 
-  const handleCheck = useCallback(
-    (checked: boolean): void => {
-      if (checked) {
-        setTheme('dark')
-      } else {
-        setTheme('light')
-      }
+  const saveTheme = useCallback(
+    (newTheme: Theme): void => {
+      setTheme(newTheme)
+      localStorage.setItem('okp4UserTheme', newTheme)
     },
     [setTheme]
   )
+
+  const handleCheck = useCallback(
+    (checked: boolean): void => {
+      saveTheme(checked ? 'dark' : 'light')
+    },
+    [saveTheme]
+  )
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      saveTheme('dark')
+      return
+    }
+    const loadedTheme = localStorage.getItem('userTheme')
+    if (loadedTheme && loadedTheme === 'dark') {
+      saveTheme(loadedTheme)
+    }
+  }, [saveTheme, setTheme])
 
   const switchIcon = isDarkTheme ? <SunIcon /> : <MoonIcon />
 
