a01741fd214f8ba6ab7270d2e0ecfcaf1de364dc
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-01 14:34:20 +0200

    feat(component): add svg icons and use them as react components in theme switcher

--- src/ui/atoms/Theme/ThemeSwitcher.tsx
+++ src/ui/atoms/Theme/ThemeSwitcher.tsx
@@ -1,12 +1,14 @@
 import React, { useCallback } from 'react'
 import * as Switch from '@radix-ui/react-switch'
-
+import MoonIcon from '../../../assets/icons/moonIcon.svg'
+import SunIcon from '../../../assets/icons/sunIcon.svg'
 import type { ThemeContextType } from './themeContext'
-import { useTheme } from './useTheme'
+import { useTheme } from '@hooks/useTheme'
 import './themeSwitcher.scss'
 
-export const ThemeSetter: React.FC = () => {
+export const ThemeSwitcher: React.FC = () => {
   const { theme, setTheme }: ThemeContextType = useTheme()
+  const isDarkTheme = theme === 'dark'
 
   const handleCheck = useCallback(
     (checked: boolean): void => {
@@ -19,12 +21,15 @@ export const ThemeSetter: React.FC = () => {
     [setTheme]
   )
 
+  const switchIcon = isDarkTheme ? <SunIcon /> : <MoonIcon />
+
   return (
     <Switch.Root
       checked={theme === 'dark'}
       className="okp4-theme-switcher-main"
       onCheckedChange={handleCheck}
     >
+      <div className={`okp4-theme-switcher-icon ${theme}`}>{switchIcon}</div>
       <Switch.Thumb className="okp4-theme-switcher-thumb" />
     </Switch.Root>
   )
