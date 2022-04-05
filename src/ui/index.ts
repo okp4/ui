b5db024be3d6dcaae9f860a4f4218f8fc8b9134b
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-05 11:57:17 +0200

    feat(project): export Logo component to lib

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,5 @@
 export * from './atoms/button/Button'
 export * from './atoms/canvas/Canvas'
+export * from './atoms/logo/Logo'
 export * from './atoms/theme/ThemeProvider'
 export * from './atoms/theme/ThemeSwitcher'
