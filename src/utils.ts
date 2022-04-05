19d319d23f2378092655835f4e34269f8b42505b
Christophe Camel <camel.christophe@gmail.com>
2022-04-05 20:24:45 +0200

    feat: add asMutable function (as the inverse of asImmutable)

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
 import type { DeepReadonly } from './superTypes'
 
 export const asImmutable = <T>(o?: T): DeepReadonly<T> => o as DeepReadonly<T>
+
+export const asMutable = <T, U extends DeepReadonly<T>>(o?: U): T => o as T
