61bbb2addefada75f54bec25dc8448522a26c923
Christophe Camel <camel.christophe@gmail.com>
2022-04-06 10:09:24 +0200

    chore(project): expose window variable to jest tests

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^domain(.*)$': '<rootDir>/src/domain/$1',
+    '^domain(.*)$': '<rootDir>/src/domain/$1'
   },
+  globals: {
+    window: {}
+  }
 }
