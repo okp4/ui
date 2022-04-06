bb34c59ec246ef195627a6a1999308f151cc1a97
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-06 15:14:35 +0200

    feat(core): add service to check bech32 address

---
+++ src/domain/faucet/services/checkOKP4Address.ts
@@ -0,0 +1,14 @@
+import { decode } from 'bech32-buffer'
+import type bech32 from 'bech32-buffer'
+import { ValidationError } from '../entities/error'
+
+export const checkOKP4Address = (address: string): void => {
+  try {
+    const { prefix }: bech32.DecodeResult = decode(address)
+    if (prefix !== 'okp4') {
+      throw new ValidationError('Address prefix does not begin with OKP4')
+    }
+  } catch (error: unknown) {
+    throw new ValidationError((error as Error).message)
+  }
+}
