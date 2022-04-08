3617c3cf935b7e2c6e44d31e12f04deabba9a2e2
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-08 11:39:00 +0200

    feat(stories): add env file to init Faucet story args

---
+++ stories/organisms/faucet/env.ts
@@ -0,0 +1,48 @@
+export const env = {
+  chainId: 'okp4-testnet-1',
+  faucetUrl: 'https://faucet.testnet.staging.okp4.network/',
+  kepkrChainInfo: [
+    {
+      chainId: 'okp4-testnet-1',
+      chainName: 'OKP4',
+      rpc: 'https://api.testnet.staging.okp4.network:443/rpc',
+      rest: 'https://api.testnet.staging.okp4.network',
+      bip44: {
+        coinType: 118
+      },
+      bech32Config: {
+        bech32PrefixAccAddr: 'okp4',
+        bech32PrefixAccPub: 'okp4' + 'pub',
+        bech32PrefixValAddr: 'okp4' + 'valoper',
+        bech32PrefixValPub: 'okp4' + 'valoperpub',
+        bech32PrefixConsAddr: 'okp4' + 'valcons',
+        bech32PrefixConsPub: 'okp4' + 'valconspub'
+      },
+      currencies: [
+        {
+          coinDenom: 'KNOW',
+          coinMinimalDenom: 'know',
+          coinDecimals: 0
+        }
+      ],
+      feeCurrencies: [
+        {
+          coinDenom: 'KNOW',
+          coinMinimalDenom: 'know',
+          coinDecimals: 0
+        }
+      ],
+      stakeCurrency: {
+        coinDenom: 'KNOW',
+        coinMinimalDenom: 'know',
+        coinDecimals: 0
+      },
+      coinType: 118,
+      gasPriceStep: {
+        low: 0.01,
+        average: 0.025,
+        high: 0.03
+      }
+    }
+  ]
+}
