5c47017bd3294021c63d7e81a6f75d9a1a25dda5
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-11 17:25:39 +0200

    feat(redux): create redux middleware to inject eventBus dispatch

--- src/domain/helpers/store.helper.ts
+++ src/domain/helpers/store.helper.ts
@@ -1,4 +1,6 @@
-import type { Action } from 'redux'
+import type { Action, Middleware, Dispatch, AnyAction } from 'redux'
+import type { DeepReadonly } from 'superTypes'
+import type { EventBus } from 'ts-bus'
 
 export interface ActionWithPayload<T extends string, P> extends Action<T> {
   readonly payload: P
@@ -18,3 +20,17 @@ type FunctionType = (...args: readonly never[]) => unknown
 type ActionCreatorsMapObject = Record<string, FunctionType>
 
 export type ActionsUnion<A extends ActionCreatorsMapObject> = ReturnType<A[keyof A]>
+
+export function createEventBusMiddleware<T>(eventBus: DeepReadonly<EventBus>): Middleware {
+  return function () {
+    return function (next: Dispatch<AnyAction>) {
+      // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+      return function (action: DeepReadonly<Action<string> | ActionWithPayload<string, T>>) {
+        const payload = (action as ActionWithPayload<string, T>).payload || null
+        const event = { type: action.type, payload: payload }
+        eventBus.publish(event)
+        return next(action)
+      }
+    }
+  }
+}
