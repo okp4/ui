3dab86c95249259fbb8b0817a9a9010ee22f0824
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-20 15:59:13 +0200

    feat(ui): enhance button component and associated style

--- src/ui/atoms/button/Button.tsx
+++ src/ui/atoms/button/Button.tsx
@@ -1,20 +1,30 @@
 import classNames from 'classnames'
 import React from 'react'
-import styles from './Button.module.scss'
+import type { DeepReadonly } from 'superTypes'
+import { Typography } from '../typography/Typography'
+import './button.scss'
 
-export type TButtonProps = Readonly<{
+export type TButtonProps = DeepReadonly<{
   /**
-   * Is this the principal call to action on the page?
+   * The variant of the main button design
    */
-  readonly primary?: boolean
+  readonly variant?: 'primary' | 'secondary'
   /**
    * How large should the button be?
    */
   readonly size?: 'small' | 'medium' | 'large'
   /**
+   * The color of the component, regarding the current theme and following semantic color naming.
+   */
+  readonly backgroundColor?: 'primary' | 'secondary' | 'success' | 'warning' | 'error' | 'info'
+  /**
    * Button contents.
    */
   readonly label: string
+  /**
+   * If true, the component is disabled.
+   */
+  readonly disabled?: boolean
   /**
    * The callback function called when button is clicked.
    * The `onClick` function is an event handler attached to the button just like a normal HTML
@@ -27,24 +37,29 @@ export type TButtonProps = Readonly<{
  * Primary UI component for user interaction.
  */
 export const Button: React.FC<TButtonProps> = ({
-  primary = false,
+  variant = 'primary',
   size = 'medium',
+  backgroundColor = 'primary',
+  disabled = false,
   label,
   onClick,
   ...props
 }: TButtonProps): JSX.Element => (
   <button
-    className={classNames(styles.core, {
-      [styles.normal]: !primary,
-      [styles.primary]: primary,
-      [styles.small]: size == 'small',
-      [styles.medium]: size == 'medium',
-      [styles.large]: size == 'large'
+    className={classNames('okp4-button-main', {
+      [`variant-${variant}`]: true,
+      [`size-${size}`]: true,
+      [`background-color-${backgroundColor}`]: true,
+      disabled: !!disabled
     })}
+    disabled={disabled}
     onClick={onClick}
+    title={label}
     type="button"
     {...props}
   >
-    {label}
+    <Typography as="div" color="highlighted-text" fontSize="small" fontWeight="light" noWrap>
+      {label}
+    </Typography>
   </button>
 )
