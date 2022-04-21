7dd3fd2456f639bee396b6451480cd364bd845ae
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-21 14:58:07 +0200

    feat(core): add error domain entity

---
+++ src/domain/error/entity/error.ts
@@ -0,0 +1,15 @@
+import type { Map } from 'immutable'
+import type { Entity } from 'domain/type/entity.type'
+
+export type Id = string
+export type Error = Entity<
+  {
+    readonly name: string
+    readonly message: string
+    readonly timeStamp: Date
+    readonly type: string
+  },
+  Id
+>
+
+export type ErrorsById = Map<Id, Error>
