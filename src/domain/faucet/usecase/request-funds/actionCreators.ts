2cc4d7c3ad6e01c3a718c253aa81a3706ad6118c
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-26 14:36:44 +0200

    feat(core): remove own error management into faucet domain

--- src/domain/faucet/usecase/request-funds/actionCreators.ts
+++ src/domain/faucet/usecase/request-funds/actionCreators.ts
@@ -1,13 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import type { ActionsUnion } from 'domain/common/store.helper'
 import { createAction } from 'domain/common/store.helper'
-import type { ValidationError, GatewayError } from 'domain/faucet/entity/error'
-import type { DeepReadonly } from 'superTypes'
 
 export const RequestFundsActions = {
   requestFundsProceeded: () => createAction('faucet/requestFundsProceeded'),
-  requestFundsFailed: (error: DeepReadonly<GatewayError> | DeepReadonly<ValidationError>) =>
-    createAction('faucet/RequestFundsFailed', { error }),
   requestFundsSucceeded: () => createAction('faucet/requestFundsSucceeded')
 }
 
