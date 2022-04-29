50632c31d3ad685441e764d538c42d1f262a4b2d
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-04-29 16:55:02 +0200

    feat(component): add severityLevel prop & enhance component

--- src/ui/atoms/toast/Toast.tsx
+++ src/ui/atoms/toast/Toast.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import * as ToastPrimitive from '@radix-ui/react-toast'
 import './toast.scss'
+import { Typography } from 'ui/atoms/typography/Typography'
 
 type ToastProps = Readonly<{
   isOpened: boolean
+  severityLevel: 'error' | 'success' | 'info' | 'warning'
   autoDuration?: number
-  swipeDirection?: 'right' | 'left' | 'up' | 'down'
   title?: string
   description?: string
   onOpenChange?: (isOpened: boolean) => void
@@ -14,16 +15,40 @@ type ToastProps = Readonly<{
 export const Toast: React.FC<ToastProps> = ({
   isOpened = false,
   autoDuration = 3000,
-  swipeDirection = 'left',
+  severityLevel,
   title,
   description,
   onOpenChange
 }: ToastProps) => {
   return (
-    <ToastPrimitive.Provider duration={autoDuration} swipeDirection={swipeDirection}>
-      <ToastPrimitive.Root className="okp4-toast-root" onOpenChange={onOpenChange} open={isOpened}>
-        <ToastPrimitive.Title>{title}</ToastPrimitive.Title>
-        <ToastPrimitive.Description>{description}</ToastPrimitive.Description>
+    <ToastPrimitive.Provider swipeDirection="right">
+      <ToastPrimitive.Root
+        className={`okp4-toast-root ${severityLevel}`}
+        defaultOpen={false}
+        duration={autoDuration}
+        onOpenChange={onOpenChange}
+        open={isOpened}
+      >
+        {title && (
+          <ToastPrimitive.Title asChild className="okp4-toast-title">
+            <Typography
+              as="span"
+              color="highlighted-text"
+              fontSize="small"
+              fontWeight="bold"
+              noWrap
+            >
+              {title}
+            </Typography>
+          </ToastPrimitive.Title>
+        )}
+        {description && (
+          <ToastPrimitive.Description className="okp4-toast-description">
+            <Typography as="span" color="highlighted-text" fontSize="small" fontWeight="light">
+              {description}
+            </Typography>
+          </ToastPrimitive.Description>
+        )}
       </ToastPrimitive.Root>
       <ToastPrimitive.Viewport className="okp4-toast-viewport" />
     </ToastPrimitive.Provider>
