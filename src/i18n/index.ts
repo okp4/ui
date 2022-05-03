58a09675bebea18a7e098c3cc1c2acebad9a020f
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-03 12:10:10 +0200

    feat(i18n): move resource upload from init to dedicated utils methods

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,9 +2,6 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-import translation_en from './translation_en.json'
-import translation_fr from './translation_fr.json'
-
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
@@ -40,8 +37,5 @@ i18n
       useSuspense: true
     }
   })
-
-i18n.addResourceBundle('en', 'translation', translation_en, true)
-i18n.addResourceBundle('fr', 'translation', translation_fr, true)
 
 export default i18n
---
+++ src/i18n/utils.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
+import type { JSONValue } from 'superTypes'
+import i18n from '../i18n/index'
+import translation_en from './translation_en.json'
+import translation_fr from './translation_fr.json'
+
+export type I18nResource = Readonly<{
+  readonly lng: string
+  readonly namespace: string
+  readonly resource: JSONValue
+}>
+
+export const loadTranslations = (resources: Array<I18nResource>): void => {
+  resources.forEach((resource: I18nResource) =>
+    i18n.addResourceBundle(resource.lng, resource.namespace, resource.resource, true)
+  )
+}
+
+export const loadDefaultTranslations = (): void => {
+  loadTranslations([
+    { lng: 'en', namespace: 'translation', resource: translation_en },
+    { lng: 'fr', namespace: 'translation', resource: translation_fr }
+  ])
+}
