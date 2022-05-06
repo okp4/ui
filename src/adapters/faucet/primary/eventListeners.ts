746736f98caf46273ba919d2d7b608649df817fd
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-06 09:58:15 +0200

    feat(adapter): dispath requestFunds UC in response to wallet/accountsRetrieved event

--- src/adapters/faucet/primary/eventListeners.ts
+++ src/adapters/faucet/primary/eventListeners.ts
@@ -1,13 +1,16 @@
-import { eventBus } from 'src/eventBus/eventBus'
+import type { EventBus } from 'ts-bus'
 import type { WalletAccountsRetrievedEvent } from 'domain/wallet/type/event.type'
-import { setAddress } from 'domain/faucet/usecase/set-address/setAddress'
 import type { ReduxStore } from 'domain/faucet/store/store'
+import { requestFunds } from 'domain/faucet/usecase/request-funds/requestFunds'
 import type { DeepReadonly } from 'superTypes'
 
-export const initFaucetEventListeners = (store: DeepReadonly<ReduxStore>): (() => void) =>
+export const initFaucetEventListeners = (
+  store: DeepReadonly<ReduxStore>,
+  eventBus: DeepReadonly<EventBus>
+): (() => void) =>
   eventBus.subscribe('wallet/accountsRetrieved', (event: WalletAccountsRetrievedEvent) => {
     const address = event.payload.accounts.get(0)?.address
     if (address) {
-      store.dispatch(setAddress(address))
+      store.dispatch(requestFunds(address))
     }
   })
