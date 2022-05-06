dcb0978c70d1dd9bdfc9ce384b129c228958ae86
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-06 17:00:41 +0200

    feat(core): add task entity

---
+++ src/domain/task/entity/task.ts
@@ -0,0 +1,17 @@
+import type { List } from 'immutable'
+import type { Entity } from 'domain/type/entity.type'
+
+export type TaskStatus = 'processing' | 'success' | 'error'
+
+export type Task<T = string, I = string> = Entity<
+  {
+    readonly creationDate: Date
+    readonly lastUpdateDate: Date
+    readonly type: T
+    readonly status: TaskStatus
+    readonly messageKey: string
+  },
+  I
+>
+
+export type Tasks<T = string, I = string> = List<Task<T, I>>
