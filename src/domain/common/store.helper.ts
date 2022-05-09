bd73949fe9c04ba31d3c081ccc78a8748f6c835a
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-09 10:05:25 +0200

    feat(redux): add metadata property into eventBus middleware  publish method

--- src/domain/common/store.helper.ts
+++ src/domain/common/store.helper.ts
@@ -1,6 +1,7 @@
 import type { Action, Middleware, Dispatch, AnyAction } from 'redux'
-import type { DeepReadonly } from 'superTypes'
 import type { EventBus } from 'ts-bus'
+import type { EventMetadata } from 'eventBus/eventBus'
+import type { DeepReadonly } from 'superTypes'
 
 export interface ActionWithPayload<T extends string, P> extends Action<T> {
   readonly payload: P
@@ -21,14 +22,18 @@ type ActionCreatorsMapObject = Record<string, FunctionType>
 
 export type ActionsUnion<A extends ActionCreatorsMapObject> = ReturnType<A[keyof A]>
 
-export function eventBusMiddleware(eventBus: DeepReadonly<EventBus>): Middleware {
+export function eventBusMiddleware(
+  eventBus: DeepReadonly<EventBus>,
+  initiator: string
+): Middleware {
   return function () {
     return function (next: Dispatch<AnyAction>) {
       // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
       return function (action: DeepReadonly<Action<string> | ActionWithPayload<string, unknown>>) {
         const payload = (action as ActionWithPayload<string, unknown>).payload || null
         const event = { type: action.type, payload: payload }
-        eventBus.publish(event)
+        const metadata: EventMetadata = { timestamp: new Date(), initiator }
+        eventBus.publish(event, metadata)
         return next(action)
       }
     }
