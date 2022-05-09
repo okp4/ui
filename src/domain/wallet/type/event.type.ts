520aa389d2c39753dcdd748fedf1d6498185e2aa
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-09 13:47:15 +0200

    feat(core): type published events with TypedBusEvent

--- src/domain/wallet/type/event.type.ts
+++ src/domain/wallet/type/event.type.ts
@@ -1,9 +1,10 @@
+import type { TypedBusEvent } from 'eventBus/eventBus'
 import type { DeepReadonly } from 'superTypes'
 import type { EnableWalletActions } from '../usecases/enable-wallet/actionCreators'
 
 export type WalletConnectedEvent = DeepReadonly<
   ReturnType<typeof EnableWalletActions['walletConnected']>
 >
-export type WalletAccountsRetrievedEvent = DeepReadonly<
-  ReturnType<typeof EnableWalletActions['accountsRetrieved']>
+export type WalletAccountsRetrievedEvent = TypedBusEvent<
+  DeepReadonly<ReturnType<typeof EnableWalletActions['accountsRetrieved']>>
 >
