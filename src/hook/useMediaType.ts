a491efdb132e690f01a6a094e34ed855f6f74bb5
ErikssonJoakim <j.eriksson@okp4.com>
2022-05-12 17:09:09 +0200

    feat: add event listener to media type handler

--- src/hook/useMediaType.ts
+++ src/hook/useMediaType.ts
@@ -1,1 +1,20 @@
-export const useMediaType = (query: string): boolean => window.matchMedia(query).matches
+import { useEffect, useState } from 'react'
+import type { DeepReadonly } from 'superTypes'
+
+export const useMediaType = (query: string): boolean => {
+  const [matches, setMatches]: [boolean, (value: boolean) => void] = useState<boolean>(false)
+
+  useEffect(() => {
+    const matchQueryList = window.matchMedia(query)
+
+    const handleMatch = (event: DeepReadonly<MediaQueryListEvent>): void => {
+      setMatches(event.matches)
+    }
+    matchQueryList.addEventListener('change', handleMatch)
+    return () => {
+      matchQueryList.removeEventListener('change', handleMatch)
+    }
+  }, [query])
+
+  return matches
+}
