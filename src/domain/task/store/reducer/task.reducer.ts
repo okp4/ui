8691d44801653382de7320e6ea546fa0c2786c5e
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-16 21:50:24 +0200

    feat(redux): add reducer for task/taskUpdated action

--- src/domain/task/store/reducer/task.reducer.ts
+++ src/domain/task/store/reducer/task.reducer.ts
@@ -1,9 +1,10 @@
 import { OrderedSet, OrderedMap } from 'immutable'
 import { combineReducers } from 'redux'
-import type { Task } from 'domain/task/entity/task'
+import type { Task, UpdateTask } from 'domain/task/entity/task'
 import type { ClearTaskActionTypes } from 'domain/task/usecase/clear-task/actionCreators'
 import type { ClearTaskskActionTypes } from 'domain/task/usecase/clear-tasks/actionCreators'
 import type { RegisterTaskActionTypes } from 'domain/task/usecase/register-task/actionCreators'
+import type { UpdateTaskActionTypes } from 'domain/task/usecase/update-task/actionCreators'
 import type { DeepReadonly } from 'superTypes'
 import type { TaskState } from '../appState'
 
@@ -15,20 +16,18 @@ const initialTaskState: TaskState<string, string> = {
 const task = (
   // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
   state: TaskState = initialTaskState,
-  action: DeepReadonly<RegisterTaskActionTypes | ClearTaskskActionTypes | ClearTaskActionTypes>
+  action: DeepReadonly<
+    RegisterTaskActionTypes | ClearTaskskActionTypes | ClearTaskActionTypes | UpdateTaskActionTypes
+  >
 ): TaskState => {
   switch (action.type) {
     case 'task/taskRegistered': {
-      const foundList = state.byType.get(action.payload.task.type)
+      const { id, type }: Task = action.payload
+      const foundList = state.byType.get(type)
       return {
         ...state,
-        byId: state.byId.set(action.payload.task.id, action.payload.task),
-        byType: state.byType.set(
-          action.payload.task.type,
-          foundList?.size
-            ? foundList.add(action.payload.task.id)
-            : OrderedSet([action.payload.task.id])
-        )
+        byId: state.byId.set(id, action.payload),
+        byType: state.byType.set(type, foundList?.size ? foundList.add(id) : OrderedSet([id]))
       }
     }
     case 'task/taskCleared': {
@@ -49,6 +48,16 @@ const task = (
         byId: state.byId.clear(),
         byType: state.byType.clear()
       }
+    case 'task/taskUpdated': {
+      const { id }: UpdateTask = action.payload
+      const foundTaskById = state.byId.get(id)
+      return {
+        ...state,
+        ...(foundTaskById && {
+          byId: state.byId.set(id, { ...foundTaskById, ...action.payload })
+        })
+      }
+    }
     default:
       return state
   }
@@ -56,15 +65,19 @@ const task = (
 
 const displayedTaskIds = (
   state: Readonly<OrderedSet<string>> = OrderedSet(),
-  action: DeepReadonly<RegisterTaskActionTypes | ClearTaskskActionTypes | ClearTaskActionTypes>
+  action: DeepReadonly<
+    RegisterTaskActionTypes | ClearTaskskActionTypes | ClearTaskActionTypes | UpdateTaskActionTypes
+  >
 ): OrderedSet<string> => {
   switch (action.type) {
     case 'task/taskRegistered':
-      return state.add(action.payload.task.id)
+      return state.add(action.payload.id)
     case 'task/tasksCleared':
       return state.clear()
     case 'task/taskCleared':
       return state.delete(action.payload)
+    case 'task/taskUpdated':
+      return state.add(action.payload.id)
     default:
       return state
   }
