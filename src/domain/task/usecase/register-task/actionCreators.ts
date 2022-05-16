8517b865fc57b171d4c7d4a69481bc3e68dd66e5
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-16 21:54:35 +0200

    refactor(core): remove payload as object for task/taskRegistered action

--- src/domain/task/usecase/register-task/actionCreators.ts
+++ src/domain/task/usecase/register-task/actionCreators.ts
@@ -4,7 +4,7 @@ import type { ActionsUnion } from 'domain/common/store.helper'
 import { createAction } from 'domain/common/store.helper'
 
 export const RegisterTaskActions = {
-  taskRegistered: (task: Task) => createAction('task/taskRegistered', { task })
+  taskRegistered: (task: Task) => createAction('task/taskRegistered', task)
 }
 
 export type RegisterTaskActionTypes = ActionsUnion<typeof RegisterTaskActions>
