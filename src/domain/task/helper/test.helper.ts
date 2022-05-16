e47053eb5d99102f099b0e672cffa5d1fb900272
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-16 21:53:33 +0200

    test(core): add a test helper file to reuse some test logic

---
+++ src/domain/task/helper/test.helper.ts
@@ -0,0 +1,21 @@
+import type { Error } from 'domain/error/entity/error'
+import type { EventMetadata } from 'eventBus/eventBus'
+import type { Pair } from 'superTypes'
+import type { Task, UpdateTask } from '../entity/task'
+
+export type EventParameter = Pair<
+  { type: string; payload: Error | Task | UpdateTask },
+  EventMetadata
+>
+type Payload = Error | Task | UpdateTask
+
+export const getExpectedEventParameter = (
+  type: string,
+  payload: Payload,
+  date: Readonly<Date>
+): EventParameter => {
+  return [
+    { ...{ type }, ...{ payload } },
+    { initiator: 'domain:task', timestamp: date }
+  ]
+}
