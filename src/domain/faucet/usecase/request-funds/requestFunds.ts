368d2dd49255c51a9a5591669d02fa4ae08f9c9e
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-05-24 13:26:47 +0200

    feat(core): dispatch new task events in requestFunds use-case

--- src/domain/faucet/usecase/request-funds/requestFunds.ts
+++ src/domain/faucet/usecase/request-funds/requestFunds.ts
@@ -1,12 +1,31 @@
-import { ThrowErrorActions } from 'domain/common/actionCreators'
+import { ThrowErrorActions, TaskActions } from 'domain/common/actionCreators'
 import { ErrorMapper } from 'domain/error/mapper/error.mapper'
 import type { ReduxStore, ThunkResult } from 'domain/faucet/store/store'
-import { RequestFundsActions } from './actionCreators'
+import { TaskBuilder } from 'domain/task/builder/task/task.builder'
+import { UpdateTaskBuilder } from 'domain/task/builder/updateTask/updateTask.builder'
 import { checkOKP4Address } from '../../service/checkOKP4Address'
+
+const createdTask = new TaskBuilder()
+  .withMessageKey('domain.task.proceeded')
+  .withType('faucet#request-funds')
+  .build()
+
+const dispatchRequestFundsAmendedTask = (
+  dispatch: ReduxStore['dispatch'],
+  hasError?: boolean
+): void => {
+  const updatedTask = new UpdateTaskBuilder()
+    .withId(createdTask.id)
+    .withMessageKey(`domain.task.${hasError ? 'error' : 'success'}`)
+    .withStatus(hasError ? 'error' : 'success')
+    .build()
+  dispatch(TaskActions.taskAmended(updatedTask))
+}
 
 const dispatchRequestFundsError = (error: unknown, dispatch: ReduxStore['dispatch']): void => {
   const errorToDispatch = ErrorMapper.mapRawErrorToEntity(error)
   dispatch(ThrowErrorActions.errorThrown(errorToDispatch))
+  dispatchRequestFundsAmendedTask(dispatch, true)
 }
 
 export const requestFunds =
@@ -15,9 +34,9 @@ export const requestFunds =
   async (dispatch, _getState, { faucetGateway }) => {
     try {
       checkOKP4Address(address)
-      dispatch(RequestFundsActions.requestFundsProceeded())
+      dispatch(TaskActions.taskCreated(createdTask))
       await faucetGateway.requestFunds(address)
-      dispatch(RequestFundsActions.requestFundsSucceeded())
+      dispatchRequestFundsAmendedTask(dispatch)
     } catch (error: unknown) {
       dispatchRequestFundsError(error, dispatch)
     }
