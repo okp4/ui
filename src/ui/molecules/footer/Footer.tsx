7e2bb78e74389bbee49418726e03d3599f07d7a9
ErikssonJoakim <j.eriksson@okp4.com>
2022-05-31 16:12:20 +0200

    style: change text style

--- src/ui/molecules/footer/Footer.tsx
+++ src/ui/molecules/footer/Footer.tsx
@@ -8,8 +8,8 @@ import footer_fr from './i18n/footer_fr.json'
 import './footer.scss'
 
 const translationsToLoad: I18nResource[] = [
-  { lng: 'EN', namespace: 'footer', resource: footer_en },
-  { lng: 'FR', namespace: 'footer', resource: footer_fr }
+  { lng: 'en', namespace: 'footer', resource: footer_en },
+  { lng: 'fr', namespace: 'footer', resource: footer_fr }
 ]
 loadTranslations(translationsToLoad)
 
@@ -17,15 +17,15 @@ export const Footer: React.FC = (): JSX.Element => {
   // eslint-disable-next-line @typescript-eslint/typedef
   const { t } = useTranslation()
   return (
-    <div className={`okp4-footer-main`}>
+    <div className="okp4-footer-main">
       <div className="okp4-footer-language-container"></div>
       <div className="okp4-footer-brand-link-container">
-        <Typography as="div" color="highlighted-text" fontSize="x-small" fontWeight="light">
+        <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="xlight">
           {t('footer:brand-link')}
         </Typography>
-        <Typography as="div" color="highlighted-text" fontSize="x-small" fontWeight="bold">
+        <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="bold">
           <a href="https://okp4.network" rel="author noreferrer" target="_blank">
-            &nbsp;ØKP4
+            ØKP4
           </a>
         </Typography>
       </div>
