9d8143fea6d86604299ba052df63902e8878296a
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-06-03 11:14:42 +0200

    test: modify HTTPFaucetGateway test suite to graphql protocol

--- src/adapters/faucet/secondary/graphql/HTTPFaucetGateway.spec.ts
+++ src/adapters/faucet/secondary/graphql/HTTPFaucetGateway.spec.ts
@@ -1,70 +1,104 @@
-import axios from 'axios'
-import type { AxiosError } from 'axios'
-import { GatewayError, UnspecifiedError } from 'domain/wallet/entities/errors'
+import {
+  createClient,
+  OperationContext,
+  OperationResult,
+  PromisifiedSource,
+  TypedDocumentNode
+} from '@urql/core'
+import * as client from './client'
+import { GatewayError, UnspecifiedError } from 'domain/faucet/entity/error'
+import * as SEND_TOKENS_MUTATION from './documents/sendTokens.graphql'
+import { MSendTokensMutation, MSendTokensMutationVariables } from './generated/types'
 import { HTTPFaucetGateway } from './HTTPFaucetGateway'
+import { DocumentNode } from 'graphql'
 
-const mockedAxiosGet = jest.spyOn(axios, 'get')
-const testUrl = 'http://my-test-fake-url.com'
-
-afterEach(() => {
-  jest.clearAllMocks()
+const fakeFaucetUrl = 'http://my-awesome-fake-url.com'
+const fakeAddress = 'okp4196877dj4crpxmja2ww2hj2vgy45v6uspkzkt8l'
+const fakeTxHash = '4ade687e8d9b734a91d08926b950910632e191b836c205cb3280f09f4c4c6fb6'
+const fakeUrqlClient = createClient({
+  url: fakeFaucetUrl,
+  requestPolicy: 'network-only'
 })
+const fakeOperation = {
+  context: {
+    url: fakeFaucetUrl,
+    requestPolicy: 'network-only'
+  },
+  key: 'hashKey#1',
+  kind: 'mutation',
+  query: SEND_TOKENS_MUTATION
+}
+let fakeUrqlMutationFn: jest.SpyInstance<
+  PromisifiedSource<OperationResult<unknown, object>>,
+  [
+    query: string | DocumentNode | TypedDocumentNode<unknown, object>,
+    variables?: object | undefined,
+    context?: Partial<OperationContext> | undefined
+  ]
+>
 
-describe('Given an HTTPFaucetGateway instance', () => {
-  const gateway = new HTTPFaucetGateway(testUrl)
+jest.spyOn(client, 'default').mockImplementation(() => fakeUrqlClient)
 
-  describe('And an axios client returning a success value', () => {
-    mockedAxiosGet.mockResolvedValueOnce(undefined)
-
-    describe('When requesting funds', () => {
-      test('Then expect the call to succeed', async () => {
-        const result = await gateway.requestFunds(testUrl)
-        expect(mockedAxiosGet).toHaveBeenCalledWith(testUrl, {
-          params: {
-            address: testUrl
-          }
+describe('Given a HTTPFaucetGateway instance', () => {
+  const gateway = new HTTPFaucetGateway(fakeFaucetUrl)
+  describe.each`
+    hasError | expectedData
+    ${false} | ${{ send: { hash: fakeTxHash } }}
+    ${false} | ${undefined}
+    ${true}  | ${undefined}
+  `(
+    `And given that expectedData is <$expectedData> and error is <$hasError>`,
+    ({ hasError, expectedData }) => {
+      describe('When requesting funds', () => {
+        beforeEach(() => {
+          fakeUrqlMutationFn = jest.spyOn(fakeUrqlClient, 'mutation').mockImplementation(
+            (): PromisifiedSource<
+              OperationResult<MSendTokensMutation, MSendTokensMutationVariables>
+              // @ts-ignore
+            > => ({
+              toPromise: () =>
+                Promise.resolve({
+                  operation: fakeOperation,
+                  data: expectedData,
+                  ...(hasError && {
+                    error: {
+                      name: 'test-error',
+                      message: 'test-message',
+                      graphQLErrors: [],
+                      networkError: new Error()
+                    }
+                  })
+                })
+            })
+          )
         })
-        expect(result).toEqual(undefined)
-        expect(mockedAxiosGet).toHaveBeenCalledTimes(1)
+
+        afterEach(() => {
+          fakeUrqlMutationFn.mockClear()
+        })
+
+        test(`Then expect the sendTokens mutation to ${
+          hasError || !expectedData ? 'fail' : 'succeed'
+        }`, async () => {
+          if (hasError) {
+            await expect(gateway.requestFunds(fakeAddress)).rejects.toThrowError(
+              new GatewayError('test-message')
+            )
+          } else if (!hasError && !expectedData) {
+            await expect(gateway.requestFunds(fakeAddress)).rejects.toThrowError(
+              new UnspecifiedError(
+                'Oooops... An unspecified error occured while requesting funds..'
+              )
+            )
+          } else {
+            await expect(gateway.requestFunds(fakeAddress)).resolves.toStrictEqual(fakeTxHash)
+          }
+          expect(fakeUrqlMutationFn).toHaveBeenCalledTimes(1)
+          expect(fakeUrqlMutationFn).toHaveBeenCalledWith(SEND_TOKENS_MUTATION, {
+            input: { toAddress: fakeAddress }
+          })
+        })
       })
-    })
-  })
-})
-
-describe('Given an HTTPFaucetGateway instance', () => {
-  const gateway = new HTTPFaucetGateway(testUrl)
-
-  describe('And an axios client returning an Axios error', () => {
-    const error = new Error('Network Error') as AxiosError
-    error.isAxiosError = true
-    mockedAxiosGet.mockRejectedValueOnce(error)
-
-    describe('When requesting funds', () => {
-      test('Then expect the call to throw a GatewayError', async () => {
-        const result = gateway.requestFunds(testUrl)
-
-        const gatewayError = new GatewayError('Network Error')
-        await expect(result).rejects.toEqual(gatewayError)
-        await expect(result).rejects.toThrow(gatewayError)
-        expect(mockedAxiosGet).toHaveBeenCalledTimes(1)
-      })
-    })
-  })
-
-  describe('And an axios client returns a non Axios error', () => {
-    const error = new Error()
-    mockedAxiosGet.mockRejectedValueOnce(error)
-
-    describe('When requesting funds', () => {
-      test('Then expect the call to throw a UnspecifiedError', async () => {
-        const unspecifiedError = new UnspecifiedError(
-          'Oooops... An unspecified error occured while requesting funds..'
-        )
-        const result = gateway.requestFunds(testUrl)
-        expect(result).rejects.toEqual(unspecifiedError)
-        expect(result).rejects.toThrow(unspecifiedError)
-        expect(mockedAxiosGet).toHaveBeenCalledTimes(1)
-      })
-    })
-  })
+    }
+  )
 })
