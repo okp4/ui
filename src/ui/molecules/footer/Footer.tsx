24c583eaf84c601c2707a7365c23da490cd69f90
ErikssonJoakim <j.eriksson@okp4.com>
2022-06-09 13:41:08 +0200

    feat: add language switcher

--- src/ui/molecules/footer/Footer.tsx
+++ src/ui/molecules/footer/Footer.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import type { Languages } from '../../../i18n/types'
 import { Typography } from 'ui/atoms/typography/Typography'
+import { LanguageSwitcher } from '../../atoms/languageSwitcher/LanguageSwitcher'
 import type { I18nResource } from '../../../i18n/utils'
 import { loadTranslations } from '../../../i18n/utils'
 import { useTranslation } from '../../../hook/useTranslation'
 import footer_en from './i18n/footer_en.json'
 import footer_fr from './i18n/footer_fr.json'
 import './footer.scss'
+
+const languages: Languages = [
+  {
+    name: 'English',
+    lng: 'en'
+  },
+  {
+    name: 'Français',
+    lng: 'fr'
+  }
+]
 
 const translationsToLoad: I18nResource[] = [
   { lng: 'en', namespace: 'footer', resource: footer_en },
@@ -18,12 +31,14 @@ export const Footer: React.FC = (): JSX.Element => {
   const { t } = useTranslation()
   return (
     <div className="okp4-footer-main">
-      <div className="okp4-footer-language-container"></div>
+      <div className="okp4-footer-language-container">
+        <LanguageSwitcher languages={languages} />
+      </div>
       <div className="okp4-footer-brand-link-container">
-        <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="xlight">
+        <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="xlight" noWrap>
           {t('footer:brand-link')}
         </Typography>
-        <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="bold">
+        <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="bold" noWrap>
           <a href="https://okp4.network" rel="author noreferrer" target="_blank">
             ØKP4
           </a>
