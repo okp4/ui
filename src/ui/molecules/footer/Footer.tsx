ae85cce4fc25dda10a554bd53a8ea7d07954c5f2
ErikssonJoakim <j.eriksson@okp4.com>
2022-06-14 10:32:05 +0200

    feat: generalise brand link & rename css class

--- src/ui/molecules/footer/Footer.tsx
+++ src/ui/molecules/footer/Footer.tsx
@@ -3,12 +3,24 @@ import type { Languages } from '../../../i18n/types'
 import { Typography } from 'ui/atoms/typography/Typography'
 import { LanguageSwitcher } from '../../atoms/languageSwitcher/LanguageSwitcher'
 import type { I18nResource } from '../../../i18n/utils'
+import type { UseTranslationResponse } from 'hook/useTranslation'
+import type { DeepReadonly } from 'superTypes'
 import { loadTranslations } from '../../../i18n/utils'
 import { useTranslation } from '../../../hook/useTranslation'
 import footer_en from './i18n/footer_en.json'
 import footer_fr from './i18n/footer_fr.json'
 import './footer.scss'
 
+type FooterProps = DeepReadonly<{
+  /**
+   * The brand link.
+   */
+  readonly brandHyperLink: Readonly<string>
+  /**
+   * The brand link.
+   */
+  readonly brandName: Readonly<string>
+}>
 const languages: Languages = [
   {
     name: 'English',
@@ -26,20 +38,22 @@ const translationsToLoad: I18nResource[] = [
 ]
 loadTranslations(translationsToLoad)
 
-export const Footer: React.FC = (): JSX.Element => {
-  // eslint-disable-next-line @typescript-eslint/typedef
-  const { t } = useTranslation()
+export const Footer: React.FC<FooterProps> = ({
+  brandHyperLink: brandLink,
+  brandName
+}: FooterProps): JSX.Element => {
+  const { t }: UseTranslationResponse = useTranslation()
   return (
     <div className="okp4-footer-main">
-      <div className="okp4-footer-language-container">
+      <div className="okp4-footer-languages-container">
         <LanguageSwitcher languages={languages} />
       </div>
       <div className="okp4-footer-brand-link-container">
         <Typography as="p" color="highlighted-text" fontSize="x-small" fontWeight="xlight">
           {t('footer:brand-link')}{' '}
-          <Typography color="highlighted-text" fontSize="x-small" fontWeight="bold" noWrap>
-            <a href="https://okp4.network" rel="author noreferrer" target="_blank">
-              ØKP4
+          <Typography color="highlighted-text" fontSize="x-small" fontWeight="bold">
+            <a href={brandLink} rel="author noreferrer" target="_blank">
+              {brandName}
             </a>
           </Typography>
         </Typography>
