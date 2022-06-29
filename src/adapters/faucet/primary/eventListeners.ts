f44fb91df237a4efb062b1b162300a7d74bda9ee
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-06-29 14:31:36 +0200

    refactor(adapters): change useCase to requestFunds in faucet event listeners

--- src/adapters/faucet/primary/eventListeners.ts
+++ src/adapters/faucet/primary/eventListeners.ts
@@ -1,8 +1,8 @@
 import type { EventBus } from 'ts-bus'
 import type { WalletAccountsRetrievedEvent } from 'domain/wallet/type/event.type'
-import { setAddress } from 'domain/faucet/usecase/set-address/setAddress'
 import type { ReduxStore } from 'domain/faucet/store/store'
 import type { DeepReadonly } from 'superTypes'
+import { requestFunds } from 'domain/faucet'
 
 export const initFaucetEventListeners = (
   store: DeepReadonly<ReduxStore>,
@@ -13,7 +13,7 @@ export const initFaucetEventListeners = (
     (event: DeepReadonly<WalletAccountsRetrievedEvent>) => {
       const address = event.payload.accounts.get(0)?.address
       if (address) {
-        store.dispatch(setAddress(address))
+        store.dispatch(requestFunds(address))
       }
     }
   )
