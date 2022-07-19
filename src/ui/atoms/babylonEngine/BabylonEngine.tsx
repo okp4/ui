3d3740e4a0a3430d3d52a18aa8801f68e16e2f4e
ccamel <camel.christophe@gmail.com>
2022-07-19 17:51:02 +0200

    feat: add babylonjs engine atom

---
+++ src/ui/atoms/babylonEngine/BabylonEngine.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useRef, useState } from 'react'
+import type { EngineOptions, Nullable } from '@babylonjs/core'
+import { Engine } from '@babylonjs/core'
+import { Canvas } from 'ui/atoms/canvas/Canvas'
+import type { Callback, DeepReadonly, StateHook } from 'superTypes'
+
+export type EngineCreatedCallback<STATE> = Callback<Engine, STATE>
+export type RenderCallback<STATE> = Callback<STATE, STATE>
+
+export type BabylonEngineProps<STATE> = DeepReadonly<{
+  /**
+   * Enable antialiasing.
+   */
+  antialias?: boolean
+
+  /**
+   * Defines whether to adapt to the device's viewport characteristics.
+   */
+  adaptToDeviceRatio?: boolean
+
+  /**
+   * Further options for the babylone engine.
+   */
+  options?: EngineOptions
+
+  /**
+   * Defines if "touch-action: 'none'" is on the canvas.
+   */
+  touchActionNone?: boolean
+
+  /**
+   * Disable scrolling on the canvas element so that it doesn't interfere with
+   * zoom in/out actions.
+   */
+  disableScrolling?: boolean
+
+  /**
+   * Width of the canvas.
+   */
+  width?: number
+
+  /**
+   * Height of the canvas.
+   */
+  height?: number
+
+  /**
+   * Called when engine is created. Can be used to configure the engine afterwards.
+   */
+  onEngineCreated?: EngineCreatedCallback<STATE>
+
+  /**
+   * Called when rendering.
+   */
+  onRender?: RenderCallback<STATE>
+}>
+
+// eslint-disable-next-line max-lines-per-function
+export const BabylonEngine = <STATE,>({
+  antialias = false,
+  adaptToDeviceRatio = false,
+  touchActionNone = true,
+  disableScrolling = true,
+  width,
+  height,
+  options,
+  onEngineCreated,
+  onRender,
+  ...props
+}: BabylonEngineProps<STATE>): JSX.Element => {
+  const [canvas, handleCanvas]: StateHook<HTMLCanvasElement | null> =
+    useState<HTMLCanvasElement | null>(null)
+  const [engine, setEngine]: StateHook<Engine | null> = useState<Engine | null>(null)
+  const stateRef = useRef<Nullable<STATE>>()
+
+  useEffect(() => {
+    if (canvas) {
+      const engine = new Engine(canvas, antialias, options, adaptToDeviceRatio)
+      setEngine(engine)
+
+      if (onEngineCreated) {
+        stateRef.current = onEngineCreated(engine)
+      }
+
+      return () => {
+        engine.dispose()
+        setEngine(null)
+      }
+    }
+  }, [adaptToDeviceRatio, antialias, canvas, onEngineCreated, options])
+
+  useEffect(() => {
+    if (engine) {
+      engine.runRenderLoop(() => {
+        if (onRender && stateRef.current) {
+          stateRef.current = onRender(stateRef.current)
+        }
+      })
+    }
+  }, [engine, onRender])
+
+  useEffect(() => {
+    engine?.resize()
+  }, [engine, width, height])
+
+  return (
+    <Canvas
+      {...props}
+      animated={false}
+      disableScrolling={disableScrolling}
+      height={height}
+      onCanvasCreated={handleCanvas}
+      touchActionNone={touchActionNone}
+      width={width}
+    />
+  )
+}
