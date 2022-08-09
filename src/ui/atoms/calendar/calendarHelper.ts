8bda95f8b6f192893930d7512ae9c9594c626535
Joey <joey.remion@gmail.com>
2022-08-09 10:48:22 +0200

    feat: create utils function to build the calendar
    feat: create utils function to build the calendar

---
+++ src/ui/atoms/calendar/calendarHelper.ts
@@ -0,0 +1,96 @@
+export type Day = 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday'
+
+export const DAYS: Day[] = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday'
+]
+
+export const WEEKS = [1, 2, 3, 4, 5, 6]
+
+export const MONTHS = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december'
+]
+
+export const getMonthStart = (year: number, month: number): number => new Date(year, month).getDay()
+
+export const getNumberOfDays = (year: number, month: number): number =>
+  42 - new Date(year, month, 42).getDate()
+
+export type DayCalendar = {
+  dateOfMonth: number
+  month: number
+  timestamp: number
+}
+
+export type WeekCalendar = DayCalendar[]
+
+export type MonthCalendar = WeekCalendar[]
+
+export const getDayCalendar = (
+  year: number,
+  month: number,
+  indexInMonth: number,
+  firstDay: number,
+  numberOfDays: number
+): DayCalendar => {
+  if (indexInMonth < firstDay) {
+    const previousMonth = month === 0 ? 11 : month - 1
+    const previousYear = month === 0 ? year - 1 : year
+    const previousMonthNumberOfDays = getNumberOfDays(previousYear, previousMonth)
+    const dateOfMonth = previousMonthNumberOfDays + indexInMonth - firstDay + 1
+    return {
+      dateOfMonth,
+      month: -1,
+      timestamp: new Date(previousYear, previousMonth, dateOfMonth).getTime()
+    }
+  }
+  const dateOfMonth = ((indexInMonth - firstDay) % numberOfDays) + 1
+  if (indexInMonth - firstDay + 1 > numberOfDays) {
+    const nextMonth = month === 11 ? 0 : month + 1
+    const nextYear = month === 11 ? year + 1 : year
+    return {
+      dateOfMonth,
+      month: +1,
+      timestamp: new Date(nextYear, nextMonth, dateOfMonth).getTime()
+    }
+  }
+  return {
+    dateOfMonth,
+    month: 0,
+    timestamp: new Date(year, month, dateOfMonth).getTime()
+  }
+}
+
+export const getMonthCalendar = (year: number, month: number): MonthCalendar => {
+  const firstDay = getMonthStart(year, month)
+  const numberOfDays = getNumberOfDays(year, month)
+  const monthCalendar: MonthCalendar = []
+
+  WEEKS.forEach((week: number) => {
+    const weekCalendar: WeekCalendar = []
+    DAYS.forEach((_day: string, index: number) => {
+      weekCalendar.push(
+        getDayCalendar(year, month, (week - 1) * 7 + index + 1, firstDay, numberOfDays)
+      )
+    })
+    monthCalendar.push(weekCalendar)
+  })
+
+  return monthCalendar
+}
