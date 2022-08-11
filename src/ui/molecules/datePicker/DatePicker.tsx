749a96613493a58aacc5922a0ef387c2f3478dc7
Joey <joey.remion@gmail.com>
2022-08-11 15:06:44 +0200

    feat: check text input date format

--- src/ui/molecules/datePicker/DatePicker.tsx
+++ src/ui/molecules/datePicker/DatePicker.tsx
@@ -1,26 +1,45 @@
 import type { DeepReadonly, UseState } from 'superTypes'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useRef, useState } from 'react'
 import { InputBase } from 'ui/atoms/inputBase/InputBase'
 import { Icon } from 'ui/atoms/icon/Icon'
 import './datePicker.scss'
 import { Calendar } from '../calendar/Calendar'
+import type { DateFormat } from './dateHelper'
+import {
+  DateLength,
+  DateRegexTyping,
+  defaultRegexTyping,
+  isValidDate,
+  stringToDate
+} from './dateHelper'
 
 export type DatePickerProps = {
   readonly value?: Date
   readonly onChange?: (date: DeepReadonly<Date>) => void
   readonly disabled?: boolean
+  readonly format?: DateFormat
 }
 
 // eslint-disable-next-line max-lines-per-function
 export const DatePicker: React.FC<DatePickerProps> = ({
   value,
   onChange,
-  disabled = false
+  disabled = false,
+  format = 'dd/mm/yyyy'
 }: DeepReadonly<DatePickerProps>): JSX.Element => {
+  const inputElement = useRef<HTMLInputElement>(null)
   const [calendarOpened, setCalendarOpened]: UseState<boolean> = useState<boolean>(false)
   const [inputValue, setInputValue]: UseState<string> = useState<string>(
     value?.toLocaleDateString() ?? ''
   )
+  const [hasError, setError]: UseState<boolean> = useState<boolean>(false)
+
+  const regexTyping = useMemo(
+    () => DateRegexTyping.get(format) ?? defaultRegexTyping,
+
+    [format]
+  )
+
   const toggleCalendar = useCallback(() => {
     if (!disabled) {
       setCalendarOpened(!calendarOpened)
@@ -36,11 +55,24 @@ export const DatePicker: React.FC<DatePickerProps> = ({
     [onChange]
   )
 
-  // HTMLInputElement can't be readonly
-  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value)
-  }, [])
+  const handleInputChange = useCallback(
+    // lint rule bypassed because of type 'Element' is not compatible with readonly
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value
+      setError(!value.match(regexTyping))
+      setInputValue(`${value}`)
+      if (value.length === DateLength) {
+        const date = stringToDate(value, format)
+        if (isValidDate(date)) {
+          onChange?.(date)
+        } else {
+          setError(true)
+        }
+      }
+    },
+    [regexTyping, format, onChange]
+  )
 
   const CalendarIcon = (): JSX.Element => (
     <div className="okp4-date-picker-icon" onClick={toggleCalendar}>
@@ -52,7 +84,11 @@ export const DatePicker: React.FC<DatePickerProps> = ({
     <div className="okp4-date-picker-main">
       <InputBase
         disabled={disabled}
-        onChange={handleChange}
+        hasError={hasError}
+        inputRef={inputElement}
+        maxLength={DateLength}
+        onChange={handleInputChange}
+        placeholder={format}
         rightIcon={CalendarIcon()}
         value={inputValue}
       />
