8ccf30fdde8834af161c9903e8541beb2b12542c
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-18 15:06:12 +0200

    feat(core): add builder and mapper in saveFiles usecase

--- src/domain/file/usecase/save-files/saveFiles.ts
+++ src/domain/file/usecase/save-files/saveFiles.ts
@@ -1,21 +1,31 @@
-import type { ThunkResult } from 'domain/file/store/store'
+import type { ReduxStore, ThunkResult } from 'domain/file/store/store'
 import type { SaveFilePayload, SaveFilesPayload } from 'domain/file/command/saveFiles'
 import { SaveFilesActions } from './actionCreators'
-import type { FileEntity } from 'domain/file/entity/file'
 import type { DeepReadonly } from 'superTypes'
+import { FileMapper } from 'domain/file/mapper/file.mapper'
+import { ErrorMapper } from 'domain/error/mapper/error.mapper'
+import { ThrowErrorActions } from 'domain/common/actionCreators'
+import { UnspecifiedError } from 'domain/file/entity/error'
+
+const dispatchError = (error: unknown, dispatch: ReduxStore['dispatch']): void => {
+  const errorToDispatch = ErrorMapper.mapRawErrorToEntity(error)
+  dispatch(ThrowErrorActions.errorThrown(errorToDispatch))
+}
 
 export const saveFiles =
   (files: DeepReadonly<SaveFilesPayload<string>>): ThunkResult<Promise<void>> =>
   // eslint-disable-next-line @typescript-eslint/typedef
-  async dispatch => {
+  async (dispatch, getState) => {
     files.forEach((file: DeepReadonly<SaveFilePayload<string>>) => {
-      const f: FileEntity = {
-        id: file.id,
-        name: file.name,
-        size: file.size,
-        type: file.type,
-        stream: file.stream
+      if (getState().file.byId.has(file.id)) {
+        dispatchError(
+          new UnspecifiedError(
+            `Oops.. The provided id '${file.id}' already exists, so we can't save this file..`
+          ),
+          dispatch
+        )
+        return
       }
-      dispatch(SaveFilesActions.fileSaved(f))
+      dispatch(SaveFilesActions.fileSaved(FileMapper.mapCommandPayloadToEntity(file)))
     })
   }
