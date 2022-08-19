13ddbd8b0654883fc37e20cf04f56505ba0d5eef
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-19 15:37:53 +0200

    refactor(builder): improve build invariant

--- src/domain/file/builder/file.builder.ts
+++ src/domain/file/builder/file.builder.ts
@@ -16,56 +16,63 @@ export class FileBuilder {
     }
   }
 
-  public withId(id: string): FileBuilder {
+  public readonly withId = (id: string): FileBuilder => {
     if (!id.length) {
       throw new UnspecifiedError('Ooops... An Id must be provided to build a file...')
     }
     return new FileBuilder({ ...this.file, id })
   }
 
-  public withName(name: string): FileBuilder {
+  public readonly withName = (name: string): FileBuilder => {
     if (!name.length) {
       throw new UnspecifiedError('Ooops... A name must be provided to build a file...')
     }
     return new FileBuilder({ ...this.file, name })
   }
 
-  public withSize(size: number): FileBuilder {
+  public readonly withSize = (size: number): FileBuilder => {
     if (!size) {
       throw new UnspecifiedError('Ooops... A size must be provided to build a file...')
     }
     return new FileBuilder({ ...this.file, size })
   }
 
-  public withType(type: string): FileBuilder {
+  public readonly withType = (type: string): FileBuilder => {
     if (!type.length) {
       throw new UnspecifiedError('Ooops... A type must be provided to build a file...')
     }
     return new FileBuilder({ ...this.file, type })
   }
 
-  public withStream(stream: DeepReadonly<ReadableStream>): FileBuilder {
+  public readonly withStream = (stream: DeepReadonly<ReadableStream>): FileBuilder => {
     if (!(stream instanceof ReadableStream)) {
       throw new UnspecifiedError('Ooops... A stream must be provided to build a file...')
     }
     return new FileBuilder({ ...this.file, stream })
   }
 
-  public build(): FileEntity {
-    if (
-      this.file.id &&
-      this.file.id.length > 0 &&
-      this.file.name &&
-      this.file.name.length > 0 &&
-      this.file.size &&
-      this.file.size > 0 &&
-      this.file.stream &&
-      this.file.stream instanceof ReadableStream &&
-      this.file.type &&
-      this.file.type.length > 0
-    ) {
+  public readonly build = (): FileEntity => {
+    if (this.buildInvariant()) {
       return this.file as FileEntity
     }
     throw new UnspecifiedError()
   }
+
+  private readonly idInvariant = (): boolean => !!this.file.id && this.file.id.length > 0
+
+  private readonly nameInvariant = (): boolean => !!this.file.name && this.file.name.length > 0
+
+  private readonly sizeInvariant = (): boolean => !!this.file.size && this.file.size > 0
+
+  private readonly streamInvariant = (): boolean =>
+    !!this.file.stream && this.file.stream instanceof ReadableStream
+
+  private readonly typeInvariant = (): boolean => !!this.file.type && this.file.type.length > 0
+
+  private readonly buildInvariant = (): boolean =>
+    this.idInvariant() &&
+    this.nameInvariant() &&
+    this.sizeInvariant() &&
+    this.streamInvariant() &&
+    this.typeInvariant()
 }
