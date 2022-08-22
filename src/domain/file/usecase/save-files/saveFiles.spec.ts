98837be6dd4227756482e3d9bd421eff3a9a27d2
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-22 11:20:50 +0200

    test(core): add a test suite to saveFiles usecase

---
+++ src/domain/file/usecase/save-files/saveFiles.spec.ts
@@ -0,0 +1,117 @@
+import { OrderedMap, OrderedSet } from 'immutable'
+import { EventBus } from 'ts-bus'
+import type { ReduxStore } from '../../store/store'
+import { saveFiles } from './saveFiles'
+import type { FileEntity } from 'domain/file/entity/file'
+import type { AppState } from 'domain/file/store/appState'
+import type { DeepReadonly } from 'superTypes'
+import { FileBuilder } from 'domain/file/builder/file.builder'
+import { FileStoreBuilder } from 'domain/file/store/builder/store.builder'
+import { SaveFilePayload } from 'domain/file/command/saveFiles'
+
+type InitialProps = Readonly<{
+  store: ReduxStore
+}>
+
+type Data = {
+  files: FileEntity[]
+  expectedState: AppState
+}
+
+const init = (): InitialProps => {
+  const eventBus = new EventBus()
+  const store = new FileStoreBuilder().withEventBus(eventBus).build()
+  return { store }
+}
+
+describe('Save files', () => {
+  const stream = new ReadableStream()
+
+  // Commands payloads
+  const rawFile1: SaveFilePayload = {
+    id: 'id1',
+    name: 'image1',
+    size: 1000,
+    type: 'image/png',
+    stream
+  }
+
+  const rawFile2: SaveFilePayload = {
+    id: 'id2',
+    name: 'image2',
+    size: 10000,
+    type: 'image/png',
+    stream
+  }
+
+  const rawFile3: SaveFilePayload = {
+    id: 'id1',
+    name: 'report3',
+    size: 10000,
+    type: 'xls',
+    stream
+  }
+
+  // Entities
+  const file1 = new FileBuilder()
+    .withId('id1')
+    .withName('image1')
+    .withSize(1000)
+    .withType('image/png')
+    .withStream(stream)
+    .build()
+
+  const file2 = new FileBuilder()
+    .withId('id2')
+    .withName('image2')
+    .withSize(10000)
+    .withType('image/png')
+    .withStream(stream)
+    .build()
+
+  const expectedState1: AppState = {
+    file: {
+      byId: OrderedMap<string, FileEntity>(),
+      byType: OrderedMap<string, OrderedSet<string>>()
+    }
+  }
+
+  const expectedState2: AppState = {
+    file: {
+      byId: OrderedMap<string, FileEntity>().set(file1.id, file1).set(file2.id, file2),
+      byType: OrderedMap<string, OrderedSet<string>>().set(
+        file1.type,
+        OrderedSet<string>().add(file1.id).add(file2.id)
+      )
+    }
+  }
+
+  const expectedState3: AppState = {
+    file: {
+      byId: OrderedMap<string, FileEntity>().set(file1.id, file1),
+      byType: OrderedMap<string, OrderedSet<string>>().set(
+        file1.type,
+        OrderedSet<string>().add(file1.id)
+      )
+    }
+  }
+
+  describe.each`
+    files                   | expectedState
+    ${[]}                   | ${expectedState1}
+    ${[rawFile1, rawFile2]} | ${expectedState2}
+    ${[rawFile1, rawFile3]} | ${expectedState3}
+  `(
+    `Given that there are $files.length file(s) to save`,
+    ({ files, expectedState }: DeepReadonly<Data>): void => {
+      const { store }: InitialProps = init()
+
+      describe('When saving files', () => {
+        test('Then, expect state to be', async () => {
+          await store.dispatch(saveFiles(files))
+          expect(store.getState()).toStrictEqual(expectedState)
+        })
+      })
+    }
+  )
+})
