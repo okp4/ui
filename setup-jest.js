f260da61ae24596a09cfb57dabf86a2c23eb167d
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-24 12:29:32 +0200

    fix: add custom polyfill for crypto.getRandomValues

--- setup-jest.js
+++ setup-jest.js
@@ -1,1 +1,8 @@
 import 'web-streams-polyfill'
+
+const nodeCrypto = require('crypto')
+window.crypto = {
+  getRandomValues: function (buffer) {
+    return nodeCrypto.randomFillSync(buffer)
+  }
+}
