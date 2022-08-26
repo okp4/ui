49e1408ef738a6e80b55d7cc145c47af28170e2a
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-26 21:50:59 +0200

    refactor(core): use CreateTask DTO instead of entities in command payload

--- src/domain/common/actionCreators.ts
+++ src/domain/common/actionCreators.ts
@@ -4,14 +4,15 @@ import type { ActionsUnion } from 'domain/common/store.helper'
 import { createAction } from 'domain/common/store.helper'
 import type { TypedBusEvent } from 'eventBus/eventBus'
 import type { DeepReadonly } from 'superTypes'
-import type { Task, UpdateTask } from 'domain/task/entity/task'
+import type { UpdateTask } from 'domain/task/entity/task'
+import type { CreateTask } from 'domain/task/command/createTask'
 
 export const ThrowErrorActions = {
   errorThrown: (error: Error) => createAction('error/errorThrown', error)
 }
 
 export const TaskActions = {
-  taskCreated: (task: Task) => createAction('task/taskCreated', task),
+  taskCreated: (task: DeepReadonly<CreateTask>) => createAction('task/taskCreated', task),
   taskAmended: (task: UpdateTask) => createAction('task/taskAmended', task)
 }
 
--- src/domain/task/usecase/register-task/registerTask.ts
+++ src/domain/task/usecase/register-task/registerTask.ts
@@ -1,9 +1,11 @@
 import type { ReduxStore, ThunkResult } from 'domain/task/store/store'
-import type { Task } from 'domain/task/entity/task'
 import { RegisterTaskActions } from './actionCreators'
 import { ThrowErrorActions } from 'domain/common/actionCreators'
 import { ErrorMapper } from 'domain/error/mapper/error.mapper'
 import { UnspecifiedError } from 'domain/task/entity/error'
+import type { CreateTask } from 'domain/task/command/createTask'
+import { TaskMapper } from 'adapters/task/mapper/task.mapper'
+import type { DeepReadonly } from 'superTypes'
 
 const dispatchError = (error: unknown, dispatch: ReduxStore['dispatch']): void => {
   const errorToDispatch = ErrorMapper.mapRawErrorToEntity(error)
@@ -11,7 +13,7 @@ const dispatchError = (error: unknown, dispatch: ReduxStore['dispatch']): void =
 }
 
 export const registerTask =
-  (task: Task): ThunkResult<Promise<void>> =>
+  (task: DeepReadonly<CreateTask>): ThunkResult<Promise<void>> =>
   // eslint-disable-next-line @typescript-eslint/typedef
   async (dispatch, getState) => {
     if (getState().task.byId.has(task.id)) {
@@ -23,5 +25,5 @@ export const registerTask =
       )
       return
     }
-    dispatch(RegisterTaskActions.taskRegistered(task))
+    dispatch(RegisterTaskActions.taskRegistered(TaskMapper.mapCommandPayloadToEntity(task)))
   }
