5fe817a8c30240c47f0db6454ccb9fbbfa99c75d
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-29 13:35:37 +0200

    refactor(core): add AmendTask command type

--- src/domain/task/command/createTask.ts
+++ src/domain/task/command/createTask.ts
@@ -8,3 +8,10 @@ export type CreateTask<I = string, T = string> = {
   readonly messageKey: string
   readonly initiator?: string
 }
+
+export type AmendTask<I = string> = {
+  readonly id: I
+  readonly timestamp: Date
+  readonly status?: TaskStatus
+  readonly messageKey?: string
+}
