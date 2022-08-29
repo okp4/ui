6ea7900dac8f54281c6e9f1e3e3267948f21ca4d
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-29 13:46:27 +0200

    chore: remove unecessary polyfill fixed by jest v.29

--- setup-jest.js
+++ setup-jest.js
@@ -1,8 +1,1 @@
 import 'web-streams-polyfill'
-
-const nodeCrypto = require('crypto')
-window.crypto = {
-  getRandomValues: function (buffer) {
-    return nodeCrypto.randomFillSync(buffer)
-  }
-}
