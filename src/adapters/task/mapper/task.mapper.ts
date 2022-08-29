8b4a839d57a5a4a3959d1685b4f36c547e9927db
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-29 13:36:30 +0200

    feat(adapter): add TaskMapper mapAmendTaskToEntity method

--- src/adapters/task/mapper/task.mapper.ts
+++ src/adapters/task/mapper/task.mapper.ts
@@ -1,10 +1,11 @@
 import { TaskBuilder } from 'domain/task/builder/task/task.builder'
-import type { Task } from 'domain/task/entity/task'
-import type { CreateTask } from 'domain/task/command/createTask'
+import type { Task, UpdateTask } from 'domain/task/entity/task'
+import type { AmendTask, CreateTask } from 'domain/task/command/createTask'
 import type { DeepReadonly } from 'superTypes'
+import { UpdateTaskBuilder } from 'domain/task/builder/updateTask/updateTask.builder'
 
 export class TaskMapper {
-  public static readonly mapCommandPayloadToEntity = (payload: DeepReadonly<CreateTask>): Task =>
+  public static readonly mapCreateTaskToEntity = (payload: DeepReadonly<CreateTask>): Task =>
     new TaskBuilder()
       .withId(payload.id)
       .withCreationDate(payload.timestamp)
@@ -14,4 +15,12 @@ export class TaskMapper {
       .withType(payload.type)
       .withStatus(payload.status)
       .build()
+
+  public static readonly mapAmendTaskToEntity = (payload: DeepReadonly<AmendTask>): UpdateTask => {
+    // eslint-disable-next-line functional/no-let
+    let builder = new UpdateTaskBuilder().withId(payload.id).withLastUpdateDate(payload.timestamp)
+    if (payload.messageKey) builder = builder.withMessageKey(payload.messageKey)
+    if (payload.status) builder = builder.withStatus(payload.status)
+    return builder.build()
+  }
 }
