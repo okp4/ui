907c1f2bee4a64ecbf53b8e5a952c9cdf5151338
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-08-29 14:53:58 +0200

    refactor(adapter): use initiator conditionaly in mapCreateTaskToEntity mapper

--- src/adapters/task/mapper/task.mapper.ts
+++ src/adapters/task/mapper/task.mapper.ts
@@ -1,3 +1,4 @@
+/* eslint-disable functional/no-let */
 import { TaskBuilder } from 'domain/task/builder/task/task.builder'
 import type { Task, UpdateTask } from 'domain/task/entity/task'
 import type { AmendTask, CreateTask } from 'domain/task/command/createTask'
@@ -5,19 +6,19 @@ import type { DeepReadonly } from 'superTypes'
 import { UpdateTaskBuilder } from 'domain/task/builder/updateTask/updateTask.builder'
 
 export class TaskMapper {
-  public static readonly mapCreateTaskToEntity = (payload: DeepReadonly<CreateTask>): Task =>
-    new TaskBuilder()
+  public static readonly mapCreateTaskToEntity = (payload: DeepReadonly<CreateTask>): Task => {
+    let builder = new TaskBuilder()
       .withId(payload.id)
       .withCreationDate(payload.timestamp)
       .withLastUpdateDate(payload.timestamp)
-      .withInitiator(payload.initiator)
       .withMessageKey(payload.messageKey)
       .withType(payload.type)
       .withStatus(payload.status)
-      .build()
+    if (payload.initiator) builder = builder.withInitiator(payload.initiator)
+    return builder.build()
+  }
 
   public static readonly mapAmendTaskToEntity = (payload: DeepReadonly<AmendTask>): UpdateTask => {
-    // eslint-disable-next-line functional/no-let
     let builder = new UpdateTaskBuilder().withId(payload.id).withLastUpdateDate(payload.timestamp)
     if (payload.messageKey) builder = builder.withMessageKey(payload.messageKey)
     if (payload.status) builder = builder.withStatus(payload.status)
