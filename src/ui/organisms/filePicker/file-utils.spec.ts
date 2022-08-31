2bdb14db5b4429245b0d1cf1e9201072ea2daae5
Joey <joey.remion@gmail.com>
2022-08-31 09:29:14 +0200

    test(file picker): add unit tests for type check
    Add isFileTypeAccepted tests

--- src/ui/organisms/filePicker/file-utils.spec.ts
+++ src/ui/organisms/filePicker/file-utils.spec.ts
@@ -1,4 +1,37 @@
-import { areFilesAccepted } from './file-utils'
+import { areFilesAccepted, isFileTypeAccepted } from './file-utils'
+
+describe('Considering the isFileTypeAccepted() function', () => {
+  describe.each`
+    file                                                      | type            | expectedResult
+    ${{ name: 'file.jpg', type: 'image/jpg' }}                | ${''}           | ${true}
+    ${{ name: 'file.png', type: 'image/png' }}                | ${'.png'}       | ${true}
+    ${{ name: 'file.gif', type: 'image/gif' }}                | ${'image/*'}    | ${true}
+    ${{ name: 'file.pdf', type: 'application/pdf' }}          | ${'.pdf'}       | ${true}
+    ${{ name: 'file.xls', type: 'application/vnd.ms-excel' }} | ${'.xls'}       | ${true}
+    ${{ name: 'file.csv', type: 'text/csv' }}                 | ${'image/*'}    | ${false}
+    ${{ name: 'file.txt', type: 'text/plain' }}               | ${'.txt'}       | ${true}
+    ${{ name: 'file.jpg', type: 'image/jpg' }}                | ${'image/jpg'}  | ${true}
+    ${{ name: 'file.png', type: 'image/png' }}                | ${'text/plain'} | ${false}
+    ${{ name: 'file.gif', type: 'image/gif' }}                | ${'image/png'}  | ${false}
+    ${{ name: 'file.txt', type: 'text/plain' }}               | ${'.csv'}       | ${false}
+    ${{ name: 'file.txt', type: 'text/plain' }}               | ${'text/csv'}   | ${false}
+    ${{ name: 'file.pdf', type: 'application/pdf' }}          | ${'image/jpg'}  | ${false}
+    ${{ name: 'file.xls', type: 'application/vnd.ms-excel' }} | ${'image/*'}    | ${false}
+    ${{ name: 'file.csv', type: 'text/csv' }}                 | ${'image/*'}    | ${false}
+    ${{ name: 'file.txt', type: 'text/plain' }}               | ${'.gif'}       | ${false}
+  `(
+    'Given the value <$file> with the type <$type>',
+    ({ file, type, expectedResult }: { file: File; type: string; expectedResult: number }) => {
+      describe('When calling function', () => {
+        const result = isFileTypeAccepted(file, type)
+
+        test(`Then, result value is ${expectedResult}`, () => {
+          expect(result).toEqual(expectedResult)
+        })
+      })
+    }
+  )
+})
 
 describe('Considering the areFilesAccepted() function', () => {
   describe.each`
