6482eca8f0406d10215dc12354479d0e7d005fc8
Joey <joey.remion@gmail.com>
2022-09-01 13:50:23 +0200

    docs(file picker): remove useless stores

--- stories/organisms/components/filePicker/store.ts
+++ stories/organisms/components/filePicker/store.ts
@@ -1,10 +1,6 @@
 import { List } from 'immutable'
 import { FileStoreBuilder } from 'domain/file/store/builder/store.builder'
-import { ErrorStoreBuilder } from 'domain/error/store/builder/store.builder'
-import { TaskStoreBuilder } from 'domain/task/store/builder/store.builder'
 import { FileContext } from 'context/storeContext/fileContext'
-import { TaskContext } from 'context/storeContext/taskContext'
-import { ErrorContext } from 'context/storeContext/errorContext'
 import { createEventBusInstance } from 'eventBus/index'
 import type { StoreParameter } from 'ui/providers/storeProvider'
 import 'domain/error/index'
@@ -15,12 +11,4 @@ const eventBusInstance = createEventBusInstance()
 const fileStore = new FileStoreBuilder().withEventBus(eventBusInstance).build()
 const fileStoreParameter: StoreParameter = [FileContext, fileStore]
 
-// Error
-const errorStore = new ErrorStoreBuilder().withEventBus(eventBusInstance).build()
-const errorStoreParameter: StoreParameter = [ErrorContext, errorStore]
-
-// Task
-const taskStore = new TaskStoreBuilder().withEventBus(eventBusInstance).build()
-const taskStoreParameter: StoreParameter = [TaskContext, taskStore]
-
-export default List([fileStoreParameter, errorStoreParameter, taskStoreParameter])
+export default List([fileStoreParameter])
