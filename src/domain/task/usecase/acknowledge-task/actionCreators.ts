0e1c0e3df4a5ef53641bf3d6a56191a21d9a4419
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-02 16:12:20 +0200

    refactor(core): implement command & event in acknowledgeTask

--- src/domain/task/usecase/acknowledge-task/actionCreators.ts
+++ src/domain/task/usecase/acknowledge-task/actionCreators.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import type { ActionsUnion } from 'domain/common/store.helper'
 import { createAction } from 'domain/common/store.helper'
+import type { TaskAcknowledgedPayload } from 'domain/task/event/taskAcknowledged'
 
 export const AcknowledgeTaskActions = {
-  taskAcknowledged: (id: string) => createAction('task/taskAcknowledged', id)
+  taskAcknowledged: (id: Readonly<TaskAcknowledgedPayload>) =>
+    createAction('task/taskAcknowledged', id)
 }
 
 export type AcknowledgeTaskActionTypes = ActionsUnion<typeof AcknowledgeTaskActions>
