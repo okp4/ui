08ff3f27a503da1d742ff7256f4cca4261e46943
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-02 16:20:33 +0200

    test(core): update requestFunds spec

--- src/domain/faucet/usecase/request-funds/requestFunds.spec.ts
+++ src/domain/faucet/usecase/request-funds/requestFunds.spec.ts
@@ -3,12 +3,13 @@ import { EventBus } from 'ts-bus'
 import { ErrorBuilder } from 'domain/error/builder/error.builder'
 import { InMemoryFaucetGateway } from 'adapters/faucet/secondary/graphql/InMemoryFaucetGateway'
 import type { ReduxStore } from 'domain/faucet/store/store'
-import { TaskBuilder } from 'domain/task/builder/task/task.builder'
+import { TaskBuilder } from 'domain/task/builder/task.builder'
 import { EventParameter, getExpectedEventParameter } from 'domain/task/helper/test.helper'
 import { requestFunds, faucetTaskType } from './requestFunds'
 import { DeepReadonly } from 'superTypes'
-import { UpdateTaskBuilder } from 'domain/task/builder/updateTask/updateTask.builder'
 import { FaucetStoreBuilder } from 'domain/faucet/store/builder/store.builder'
+import { TaskRegisterReceivedPayload } from 'domain/task/event/taskRegisterReceived'
+import { TaskStatusAmendReceivedPayload } from 'domain/task/event/taskStatusAmendReceived'
 
 interface InitialProps {
   store: ReduxStore
@@ -18,41 +19,36 @@ interface InitialProps {
 interface Data {
   hasGatewayError: boolean
   address: string
-  expectedEventParameters: EventParameter[]
+  expectedEventParameters: EventParameter<
+    TaskRegisterReceivedPayload | TaskStatusAmendReceivedPayload
+  >[]
 }
 
 const initiator = 'domain:faucet'
 const fakedUuid = 'foobar'
-const fakedDate = new Date(1992, 1, 1)
+const fakedDate = new Date('2021-01-01T09:00:00.000Z')
 const eventBus = new EventBus()
 
-jest.useFakeTimers()
-jest.setSystemTime(fakedDate)
 short.generate = jest.fn(() => fakedUuid as short.SUUID)
 const mockedEventBusPublish = jest.spyOn(eventBus, 'publish')
 
-const task = new TaskBuilder()
-  .withId(fakedUuid)
-  .withCreationDate(fakedDate)
-  .withLastUpdateDate(fakedDate)
-  .withMessageKey('domain.task.proceeded')
-  .withType(faucetTaskType)
-  .build()
+// Event payloads
+const taskToRegister1: TaskRegisterReceivedPayload = {
+  id: fakedUuid,
+  type: faucetTaskType
+}
 
-const updatedTask1 = new UpdateTaskBuilder()
-  .withId(fakedUuid)
-  .withLastUpdateDate(fakedDate)
-  .withMessageKey('domain.task.error')
-  .withStatus('error')
-  .build()
+const taskStatusToAmend1: TaskStatusAmendReceivedPayload = {
+  id: fakedUuid,
+  status: 'error'
+}
 
-const updatedTask2 = new UpdateTaskBuilder()
-  .withId(fakedUuid)
-  .withLastUpdateDate(fakedDate)
-  .withMessageKey('domain.task.success')
-  .withStatus('success')
-  .build()
+const taskStatusToAmend2: TaskStatusAmendReceivedPayload = {
+  id: fakedUuid,
+  status: 'success'
+}
 
+// Entities
 const bech32Error = new ErrorBuilder()
   .withId(fakedUuid)
   .withTimestamp(fakedDate)
@@ -90,16 +86,12 @@ const cleanErrorStack = (): void => {
 }
 
 describe('Request funds from faucet', () => {
-  afterAll(() => {
-    jest.useRealTimers()
-  })
-
   describe.each`
     hasGatewayError | address                                            | expectedEventParameters
-    ${false}        | ${'123'}                                           | ${[getExpectedEventParameter('task/taskCreated', task, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', bech32Error, initiator, fakedDate), getExpectedEventParameter('task/taskAmended', updatedTask1, initiator, fakedDate)]}
-    ${false}        | ${'cosmos196877dj4crpxmja2ww2hj2vgy45v6uspm7nrmy'} | ${[getExpectedEventParameter('task/taskCreated', task, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', bech32Error, initiator, fakedDate), getExpectedEventParameter('task/taskAmended', updatedTask1, initiator, fakedDate)]}
-    ${true}         | ${'okp4196877dj4crpxmja2ww2hj2vgy45v6uspkzkt8l'}   | ${[getExpectedEventParameter('task/taskCreated', task, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', faucetGatewayError, initiator, fakedDate), getExpectedEventParameter('task/taskAmended', updatedTask1, initiator, fakedDate)]}
-    ${false}        | ${'okp4196877dj4crpxmja2ww2hj2vgy45v6uspkzkt8l'}   | ${[getExpectedEventParameter('task/taskCreated', task, initiator, fakedDate), getExpectedEventParameter('task/taskAmended', updatedTask2, initiator, fakedDate)]}
+    ${false}        | ${'123'}                                           | ${[getExpectedEventParameter('task/taskRegisterReceived', taskToRegister1, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', bech32Error, initiator, fakedDate), getExpectedEventParameter('task/taskStatusAmendReceived', taskStatusToAmend1, initiator, fakedDate)]}
+    ${false}        | ${'cosmos196877dj4crpxmja2ww2hj2vgy45v6uspm7nrmy'} | ${[getExpectedEventParameter('task/taskRegisterReceived', taskToRegister1, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', bech32Error, initiator, fakedDate), getExpectedEventParameter('task/taskStatusAmendReceived', taskStatusToAmend1, initiator, fakedDate)]}
+    ${true}         | ${'okp4196877dj4crpxmja2ww2hj2vgy45v6uspkzkt8l'}   | ${[getExpectedEventParameter('task/taskRegisterReceived', taskToRegister1, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', faucetGatewayError, initiator, fakedDate), getExpectedEventParameter('task/taskStatusAmendReceived', taskStatusToAmend1, initiator, fakedDate)]}
+    ${false}        | ${'okp4196877dj4crpxmja2ww2hj2vgy45v6uspkzkt8l'}   | ${[getExpectedEventParameter('task/taskRegisterReceived', taskToRegister1, initiator, fakedDate), getExpectedEventParameter('task/taskStatusAmendReceived', taskStatusToAmend2, initiator, fakedDate)]}
   `(
     'Given that address is <$address>',
     ({ hasGatewayError, address, expectedEventParameters }: Readonly<Data>): void => {
@@ -111,11 +103,20 @@ describe('Request funds from faucet', () => {
         })
         test('Then, expect events to be published', async () => {
           await store.dispatch(requestFunds(address))
-          expectedEventParameters.forEach((elt: DeepReadonly<EventParameter>, index: number) => {
-            const [first, second]: Readonly<EventParameter> = elt
-            cleanErrorStack()
-            expect(mockedEventBusPublish).toHaveBeenNthCalledWith(index + 1, first, second)
-          })
+          expectedEventParameters.forEach(
+            (
+              elt: DeepReadonly<
+                EventParameter<TaskRegisterReceivedPayload | TaskStatusAmendReceivedPayload>
+              >,
+              index: number
+            ) => {
+              const [first, second]: Readonly<
+                EventParameter<TaskRegisterReceivedPayload | TaskStatusAmendReceivedPayload>
+              > = elt
+              cleanErrorStack()
+              expect(mockedEventBusPublish).toHaveBeenNthCalledWith(index + 1, first, second)
+            }
+          )
         })
       })
     }
