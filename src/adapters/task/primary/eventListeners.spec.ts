ac9479e31dcde5ac6787543a616a7badbb2a063c
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-02 16:18:32 +0200

    test(adapter): update task event listeners spec

--- src/adapters/task/primary/eventListeners.spec.ts
+++ src/adapters/task/primary/eventListeners.spec.ts
@@ -4,11 +4,12 @@ import type { BusEvent } from 'ts-bus/types'
 import type { ReduxStore } from 'domain/task/store/store'
 import type { AppState } from 'domain/task/store/appState'
 import type { DeepReadonly } from 'superTypes'
-import { TaskBuilder } from 'domain/task/builder/task/task.builder'
+import { TaskBuilder } from 'domain/task/builder/task.builder'
 import type { EventMetadata } from 'eventBus/eventBus'
 import type { Task } from 'domain/task/entity/task'
-import { UpdateTaskBuilder } from 'domain/task/builder/updateTask/updateTask.builder'
 import { TaskStoreBuilder } from 'domain/task/store/builder/store.builder'
+import { TaskRegisterReceivedPayload } from 'domain/task/event/taskRegisterReceived'
+import { TaskStatusAmendReceivedPayload } from 'domain/task/event/taskStatusAmendReceived'
 
 type InitialProps = Readonly<{
   store: ReduxStore
@@ -20,76 +21,99 @@ type Data = DeepReadonly<{
   expectedState: AppState
 }>
 
-const aDate = new Date()
-const bDate = new Date()
-
-const task1 = new TaskBuilder()
-  .withId('id1')
-  .withCreationDate(aDate)
-  .withLastUpdateDate(aDate)
-  .withMessageKey('domain.task.processing')
-  .withType('task-test')
-  .withStatus('processing')
-  .withInitiator('domain:test')
-  .build()
-
-const task2 = new TaskBuilder()
-  .withId('id2')
-  .withCreationDate(aDate)
-  .withLastUpdateDate(aDate)
-  .withMessageKey('domain.task.processing')
-  .withType('task-test')
-  .withStatus('processing')
-  .withInitiator('domain:test')
-  .build()
-
-const updatedTask = new UpdateTaskBuilder()
-  .withId('id1')
-  .withLastUpdateDate(bDate)
-  .withMessageKey('domain.task.succeeded')
-  .withStatus('success')
-  .build()
-
-const meta: EventMetadata = { initiator: 'domain:test', timestamp: new Date() }
-const initialState: AppState = {
-  task: {
-    byId: OrderedMap<string, Task>().set(task1.id, task1),
-    byType: OrderedMap<string, OrderedSet<string>>().set(task1.type, OrderedSet<string>([task1.id]))
-  },
-  displayedTaskIds: OrderedSet<string>([task1.id])
-}
-
-const init = (): InitialProps => {
+const init = (preloadedState: AppState): InitialProps => {
   const eventBus = new EventBus()
   const store = new TaskStoreBuilder()
     .withEventBus(eventBus)
-    .withPreloadedState(initialState)
+    .withPreloadedState(preloadedState)
     .build()
   return { store, eventBus }
 }
 
-const getExpectedState = (
-  task: DeepReadonly<AppState['task']>,
-  displayedTaskIds: DeepReadonly<AppState['displayedTaskIds']>
-): AppState => ({
-  task,
-  displayedTaskIds
-})
+describe('Listen to task events', () => {
+  const fakedDate = new Date('2021-01-01T09:00:00.000Z')
 
-describe.each`
-  event                                                 | expectedState
-  ${undefined}                                          | ${getExpectedState(initialState.task, initialState.displayedTaskIds)}
-  ${{ type: 'task/fooBar', payload: task2 }}            | ${getExpectedState(initialState.task, initialState.displayedTaskIds)}
-  ${{ type: 'task/taskCreated', payload: task2 }}       | ${getExpectedState({ byId: initialState.task.byId.set(task2.id, task2), byType: initialState.task.byType.set(task2.type, initialState.task.byType.get(task2.type, OrderedSet<string>()).add(task2.id)) }, initialState.displayedTaskIds.add(task2.id))}
-  ${{ type: 'task/taskAmended', payload: updatedTask }} | ${getExpectedState({ byId: initialState.task.byId.set(task1.id, { ...task1, ...updatedTask }), byType: initialState.task.byType }, initialState.displayedTaskIds)}
-`('Given that event is <$event>', ({ event, expectedState }: Data): void => {
-  const { store, eventBus }: InitialProps = init()
+  // Event payloads
+  const taskToRegister: TaskRegisterReceivedPayload = {
+    id: 'id2',
+    type: 'task-test'
+  }
 
-  describe(`When publishing ${!event ? 'no' : `a ${event.type}`} event`, () => {
-    event && eventBus.publish(event, meta)
+  const taskStatusToAmend: TaskStatusAmendReceivedPayload = {
+    id: 'id1',
+    status: 'success'
+  }
 
-    test(`Then, expect state to be ${expectedState}`, () => {
-      expect(store.getState()).toEqual(expectedState)
+  // Entities
+  const task1 = new TaskBuilder()
+    .withId('id1')
+    .withCreationDate(fakedDate)
+    .withLastUpdateDate(fakedDate)
+    .withType('task-test')
+    .withStatus('processing')
+    .withInitiator('domain:test')
+    .build()
+
+  const task2 = new TaskBuilder()
+    .withId('id2')
+    .withCreationDate(fakedDate)
+    .withLastUpdateDate(fakedDate)
+    .withType('task-test')
+    .withStatus('processing')
+    .withInitiator('domain:test')
+    .build()
+
+  const meta: EventMetadata = { initiator: 'domain:test', timestamp: new Date() }
+
+  // States
+  const initialState: AppState = {
+    task: {
+      byId: OrderedMap<string, Task>().set(task1.id, task1),
+      byType: OrderedMap<string, OrderedSet<string>>().set(
+        task1.type,
+        OrderedSet<string>([task1.id])
+      )
+    },
+    displayedTaskIds: OrderedSet<string>([task1.id])
+  }
+
+  const expectedState1: AppState = {
+    task: {
+      byId: initialState.task.byId.set(task2.id, task2),
+      byType: initialState.task.byType.set(
+        task2.type,
+        initialState.task.byType.get(task2.type, OrderedSet<string>()).add(task2.id)
+      )
+    },
+    displayedTaskIds: initialState.displayedTaskIds.add(task2.id)
+  }
+
+  const expectedState2: AppState = {
+    task: {
+      byId: initialState.task.byId.set(task1.id, { ...task1, status: 'success' }),
+      byType: initialState.task.byType.set(
+        task1.type,
+        initialState.task.byType.get(task1.type, OrderedSet<string>()).add(task1.id)
+      )
+    },
+    displayedTaskIds: initialState.displayedTaskIds.add(task1.id)
+  }
+
+  describe.each`
+    event                                                                   | expectedState
+    ${undefined}                                                            | ${initialState}
+    ${{ type: 'task/fooBar', payload: taskToRegister }}                     | ${initialState}
+    ${{ type: 'task/taskRegisterReceived', payload: taskToRegister }}       | ${expectedState1}
+    ${{ type: 'task/taskStatusAmendReceived', payload: taskStatusToAmend }} | ${expectedState2}
+  `('Given that event is <$event>', ({ event, expectedState }: Data): void => {
+    const { store, eventBus }: InitialProps = init(initialState)
+
+    describe(`When publishing ${!event ? 'no' : `a ${event.type}`} event`, () => {
+      event && eventBus.publish(event, meta)
+
+      test(`Then, expect state to be ${expectedState}`, () => {
+        expect(store.getState()).toEqual(expectedState)
+      })
     })
   })
 })
