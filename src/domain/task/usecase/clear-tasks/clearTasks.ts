9e7cb53633e2c3d875a2d429b736b3904dfe00c2
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-02 16:14:49 +0200

    refactor(core): rename & implement command & event in removeTasks

--- src/domain/task/usecase/clear-tasks/clearTasks.ts
+++
@@ -1,9 +0,0 @@
-import type { ThunkResult } from 'domain/task/store/store'
-import { ClearTasksActions } from './actionCreators'
-
-export const clearTasks =
-  (): ThunkResult<Promise<void>> =>
-  // eslint-disable-next-line @typescript-eslint/typedef
-  async dispatch => {
-    dispatch(ClearTasksActions.tasksCleared())
-  }
