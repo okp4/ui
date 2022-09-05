eacd3c6416e875ecad866aaa7e846ad42dbbff07
lauren olivier <laurenzolivier@gmail.com>
2022-09-05 17:08:25 +0200

    refactor: remove WithNavigation & WithoutNavigation components

--- src/ui/molecules/header/Header.tsx
+++ src/ui/molecules/header/Header.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import './header.scss'
-import type { DeepReadonly } from 'superTypes'
-import { useBreakpoint } from 'hook/useBreakpoint'
-import type { Breakpoints } from 'hook/useBreakpoint'
+import React, { useCallback, useState } from 'react'
+import type { DeepReadonly, UseState } from 'superTypes'
 import classNames from 'classnames'
-import { WithNavigation } from './withNav/WithNavigation'
-import { WithoutNavigation } from './withoutNav/WithoutNavigation'
+import { useMediaType } from 'hook/useMediaType'
+import { ThemeSwitcher as Switcher } from 'ui/atoms/theme/ThemeSwitcher'
+import { Icon } from 'ui/atoms/icon/Icon'
+import './header.scss'
 
 export type HeaderProps = {
   /**
@@ -18,20 +17,88 @@ export type HeaderProps = {
   readonly navigationMenu?: JSX.Element[]
 }
 
+const BurgerMenu = ({
+  isOpened,
+  onBurgerMenuOpen
+}: DeepReadonly<{ isOpened: boolean; onBurgerMenuOpen: () => void }>): JSX.Element => (
+  <div
+    className={classNames(
+      'okp4-header-navigation-burger-menu',
+      isOpened ? 'rotate-up' : 'rotate-down'
+    )}
+    onClick={onBurgerMenuOpen}
+  >
+    <Icon name="menu" />
+  </div>
+)
+
+const BurgerMenuList = ({
+  navigationMenu
+}: DeepReadonly<{ navigationMenu: JSX.Element[] }>): JSX.Element => (
+  <div className="okp4-header-navigation-burger-menu-list">
+    {navigationMenu.map((link: Readonly<JSX.Element>, index: number) => (
+      <div className="okp4-header-navigation-burger-menu-item" key={index}>
+        {link}
+      </div>
+    ))}
+  </div>
+)
+
+const RowMenuList = ({
+  navigationMenu
+}: DeepReadonly<{ navigationMenu: JSX.Element[] }>): JSX.Element => (
+  <div className="okp4-header-navigation-menu-list">
+    {navigationMenu.map((link: Readonly<JSX.Element>, index: number) => (
+      <div className="okp4-header-navigation-menu-item" key={index}>
+        {link}
+      </div>
+    ))}
+  </div>
+)
+
+const ThemeSwitcher = (): JSX.Element => (
+  <div className="okp4-header-theme-switcher-container">
+    <Switcher />
+  </div>
+)
+
 export const Header: React.FC<HeaderProps> = ({
   firstElement,
   navigationMenu
 }: DeepReadonly<HeaderProps>): JSX.Element => {
-  const { isXSmall }: Breakpoints = useBreakpoint()
-  const containerClass = classNames('okp4-header-first-element-container', isXSmall && 'x-small')
+  const [isMenuOpened, setIsMenuOpened]: UseState<boolean> = useState<boolean>(false)
+  const showBurgerMenu = useMediaType('(max-width: 995px)')
+  const showBurgerMenuList = showBurgerMenu && isMenuOpened
+  const headerClassname = classNames(
+    'okp4-header-main',
+    navigationMenu ? 'with-navigation' : 'without-navigation',
+    {
+      'burger-list': showBurgerMenuList
+    }
+  )
 
-  return navigationMenu ? (
-    <WithNavigation
-      containerClass={containerClass}
-      firstElement={firstElement}
-      navigationMenu={navigationMenu}
-    />
-  ) : (
-    <WithoutNavigation containerClass={containerClass} firstElement={firstElement} />
+  const handleBurgerMenuOpen = useCallback((): void => {
+    setIsMenuOpened(!isMenuOpened)
+  }, [isMenuOpened, setIsMenuOpened])
+
+  return (
+    <div className={headerClassname}>
+      {navigationMenu ? (
+        <>
+          {showBurgerMenu && (
+            <BurgerMenu isOpened={isMenuOpened} onBurgerMenuOpen={handleBurgerMenuOpen} />
+          )}
+          {showBurgerMenuList && <BurgerMenuList navigationMenu={navigationMenu} />}
+          <div className="okp4-header-first-element-container">{firstElement}</div>
+          {!showBurgerMenu && <RowMenuList navigationMenu={navigationMenu} />}
+          <ThemeSwitcher />
+        </>
+      ) : (
+        <>
+          <div className="okp4-header-first-element-container">{firstElement}</div>
+          <ThemeSwitcher />
+        </>
+      )}
+    </div>
   )
 }
