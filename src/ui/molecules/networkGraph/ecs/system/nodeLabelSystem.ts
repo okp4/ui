dcac4affe164183d7cfd16298cb06c4cc9f46e09
ccamel <camel.christophe@gmail.com>
2022-09-05 21:51:26 +0200

    feat: add system to extract labels from nodes into a component

---
+++ src/ui/molecules/networkGraph/ecs/system/nodeLabelSystem.ts
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
+import type { ComponentOf } from '@javelin/ecs'
+import { component, createQuery, useMonitor } from '@javelin/ecs'
+import type { Node } from 'ngraph.graph'
+import { LabelComponent, NodeComponent } from '../schema'
+import type { SystemFactory, World, WorldTickData } from '../type'
+
+const queries = {
+  nodes: createQuery(NodeComponent)
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type LabelExtractor<T = any> = (node: Node<T>) => string | null
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type NodeLabelSystemOptions<T = any> = {
+  labelExtractor: LabelExtractor<T>
+}
+
+const defaultLabelExtractor = (node: Node<Record<string, string> | null>): string | null => {
+  return node.data?.['label'] ?? null
+}
+
+// eslint-disable-next-line max-lines-per-function
+export const NodeLabelSystem: SystemFactory<NodeLabelSystemOptions> =
+  (options?: NodeLabelSystemOptions) => (world: World) => {
+    const { graph }: WorldTickData = world.latestTickData
+    useMonitor(queries.nodes, (e: number, [n]: [ComponentOf<typeof NodeComponent>]) => {
+      const node = graph.getNode(n.id)
+      const label = node ? (options?.labelExtractor ?? defaultLabelExtractor)(node) : null
+
+      if (label) {
+        world.attach(e, component(LabelComponent, { label }))
+      }
+    })
+  }
