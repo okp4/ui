b249b02584735dc92d6bb9ca5b4a8672bc097a4e
ccamel <camel.christophe@gmail.com>
2022-09-06 20:04:26 +0200

    feat: add system to prepare the scene on first rendering

---
+++ src/ui/molecules/networkGraph/ecs/system/prepareSceneSystem.ts
@@ -0,0 +1,14 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
+import { Color3, Color4 } from '@babylonjs/core'
+import { useInit } from '@javelin/ecs'
+import type { SystemFactory, World, WorldTickData } from '../type'
+
+export const PrepareSceneSystem: SystemFactory = () => (world: World) => {
+  if (useInit()) {
+    const { scene }: WorldTickData = world.latestTickData
+
+    scene.clearColor = Color4.FromColor3(Color3.Black())
+    scene.ambientColor = Color3.FromHexString('#bbbbbb')
+  }
+}
