f8cf605df278acd8f81d2c09ece3095a520ce60b
ccamel <camel.christophe@gmail.com>
2022-09-06 20:53:26 +0200

    feat: add system to transform a graph into entities

---
+++ src/ui/molecules/networkGraph/ecs/system/spawnGraphSystem.ts
@@ -0,0 +1,78 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
+import type { Mesh, Scene } from '@babylonjs/core'
+import { Color3, MeshBuilder, StandardMaterial, Vector3 } from '@babylonjs/core'
+import { component, toComponent, useInit } from '@javelin/ecs'
+import type { Link, Node } from 'ngraph.graph'
+import { LinkComponent, MeshComponent, NodeComponent, PositionComponent } from '../schema'
+import type { SystemFactory, World, WorldTickData } from '../type'
+
+const newNodeMesh = (node: Node, scene: Scene): Mesh => {
+  const material = new StandardMaterial('material#node', scene)
+  material.diffuseColor = new Color3(1, 1, 0.7)
+  material.alpha = 0.9
+
+  const mesh = MeshBuilder.CreateSphere(
+    `node:sphere#${node.id}`,
+    {
+      diameter: 5,
+      segments: 2
+    },
+    scene
+  )
+  mesh.metadata = {
+    '@tyle': 'node',
+    nodeId: node.id as number
+  }
+  mesh.material = material
+
+  return mesh
+}
+
+const newLinkMesh = (link: Link, scene: Scene): Mesh => {
+  const mesh = MeshBuilder.CreateLines(
+    `link:line#${link.id}`,
+    {
+      points: [new Vector3(0, 0, 0), new Vector3(0, 0, 0)],
+      updatable: true
+    },
+    scene
+  )
+  mesh.color = new Color3(0.3, 0.3, 0.4)
+
+  return mesh
+}
+
+export const SpawnGraphSystem: SystemFactory = () => (world: World) => {
+  if (useInit()) {
+    const { graph, scene }: WorldTickData = world.latestTickData
+
+    const nodes: Map<number, number> = new Map()
+    graph.forEachNode((node: Node) => {
+      const id = node.id as number
+      const mesh = newNodeMesh(node, scene)
+
+      nodes.set(
+        id,
+        world.create(
+          component(NodeComponent, { id }),
+          component(PositionComponent, { x: 0, y: 0, z: 0 }),
+          toComponent(mesh, MeshComponent)
+        )
+      )
+    })
+
+    graph.forEachLink((link: Link) => {
+      const mesh = newLinkMesh(link, scene)
+
+      world.create(
+        component(LinkComponent, {
+          id: link.id,
+          fromEntity: nodes.get(link.fromId as number),
+          toEntity: nodes.get(link.toId as number)
+        }),
+        toComponent(mesh, MeshComponent)
+      )
+    })
+  }
+}
