c63d51d6afdbaa5dcf2d223d02b84c841f1c2c08
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-07 15:09:57 +0200

    fix: make client method asynchronous with event listeners

--- src/adapters/faucet/secondary/graphql/client.ts
+++ src/adapters/faucet/secondary/graphql/client.ts
@@ -1,5 +1,6 @@
 import { createClient, defaultExchanges, subscriptionExchange } from '@urql/core'
 import type { Client, ExecutionResult } from '@urql/core'
+import type { Client as GQLWSClient } from 'graphql-ws'
 import { createClient as createWSClient } from 'graphql-ws'
 import type {
   ObserverLike,
@@ -9,28 +10,54 @@ import type { DeepReadonly } from 'superTypes'
 
 const urlReg = new RegExp('^(?<scheme>[a-z][a-z0-9+\\-.]*)://(?<target>.*)$')
 
+export type FaucetClient = {
+  gqlClient: Client
+  unsuscribe: () => void
+}
+
 export const makeWSURL = (rawURL: string): string => {
   const match = urlReg.exec(rawURL)
   const wsProtocol = (): string => (match?.groups?.scheme === 'https' ? 'wss' : 'ws')
   return `${wsProtocol()}://${match?.groups?.target}`
 }
 
-const client = (url: string): Client =>
-  createClient({
-    url,
-    requestPolicy: 'network-only',
-    exchanges: [
-      ...defaultExchanges,
-      subscriptionExchange({
-        forwardSubscription: (operation: DeepReadonly<SubscriptionOperation>) => ({
-          subscribe: (sink: DeepReadonly<ObserverLike<ExecutionResult>>) => ({
-            unsubscribe: createWSClient({
-              url: makeWSURL(url)
-            }).subscribe(operation, sink)
+const client = async (url: string): Promise<FaucetClient> => {
+  return new Promise<GQLWSClient>(
+    (
+      resolve: (gqlWsClient: DeepReadonly<GQLWSClient>) => void,
+      reject: (reason: string) => void
+    ) => {
+      const wsClient = createWSClient({
+        url: makeWSURL(url),
+        lazy: false,
+        on: {
+          error: () => {
+            wsClient.terminate()
+            reject('GQLWS client failed to connect')
+          },
+          connected: () => {
+            resolve(wsClient)
+          }
+        }
+      })
+    }
+  ).then((gqlWsClient: DeepReadonly<GQLWSClient>) => ({
+    gqlClient: createClient({
+      url,
+      requestPolicy: 'network-only',
+      exchanges: [
+        ...defaultExchanges,
+        subscriptionExchange({
+          forwardSubscription: (operation: DeepReadonly<SubscriptionOperation>) => ({
+            subscribe: (sink: DeepReadonly<ObserverLike<ExecutionResult>>) => ({
+              unsubscribe: gqlWsClient.subscribe(operation, sink)
+            })
           })
         })
-      })
-    ]
-  })
+      ]
+    }),
+    unsuscribe: gqlWsClient.dispose
+  }))
+}
 
 export default client
