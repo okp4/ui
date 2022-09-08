8c78e9d676b78904e92fe1b78b488d7d5ac074c8
ErikssonJoakim <j.eriksson@okp4.com>
2022-09-08 16:48:27 +0200

    feat: pass layout property to children

--- src/ui/atoms/list/List.tsx
+++ src/ui/atoms/list/List.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames'
 import React from 'react'
 import type { DeepReadonly } from 'superTypes'
 import './list.scss'
@@ -8,7 +7,7 @@ export type ListProps = {
    * The elements passed as children of the List component.
    * The expected children are ListItem components.
    */
-  readonly children: React.ReactNode
+  readonly children: React.ReactElement[]
   /**
    * The layout of List. Is also passed to children as props.
    */
@@ -18,6 +17,12 @@ export type ListProps = {
 export const List: React.FC<ListProps> = ({
   children,
   layout = 'list'
-}: DeepReadonly<ListProps>): JSX.Element => (
-  <div className={classNames('okp4-list-main', `layout-${layout}`)}>{children}</div>
-)
+}: DeepReadonly<ListProps>): JSX.Element => {
+  const clonedChildren = React.Children.map(children, (child: DeepReadonly<React.ReactElement>) => {
+    const newProps: Pick<ListProps, 'layout'> = {
+      layout
+    }
+    return React.isValidElement(child) ? React.cloneElement(child, newProps) : child
+  })
+  return <div className={`okp4-list-main layout-${layout}`}>{clonedChildren}</div>
+}
