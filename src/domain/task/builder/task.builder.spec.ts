c100cc9995577a1e3c62c46d26fdee512edd81bc
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-09 14:09:15 +0200

    test(builder): update task.builder test suite with progress property

--- src/domain/task/builder/task.builder.spec.ts
+++ src/domain/task/builder/task.builder.spec.ts
@@ -1,6 +1,6 @@
 import short from 'short-uuid'
 import { UnspecifiedError } from '../entity/error'
-import type { Task, TaskStatus } from '../entity/task'
+import type { Task, TaskProgress, TaskStatus } from '../entity/task'
 import { TaskBuilder } from './task.builder'
 
 type Data = Readonly<
@@ -12,6 +12,7 @@ type Data = Readonly<
     type: string
     initiator: string
     status: TaskStatus
+    progress?: TaskProgress
   }> & {
     expectedStatus: boolean
   }
@@ -29,19 +30,24 @@ describe('Build a task', () => {
   })
 
   describe.each`
-    initialTask     | id           | creationDate | lastUpdateDate | type           | initiator | status          | expectedStatus
-    ${undefined}    | ${'#id1'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${true}
-    ${undefined}    | ${'#id2'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${undefined}    | ${true}
-    ${undefined}    | ${'#id3'}    | ${aDate}     | ${undefined}   | ${'test-task'} | ${'test'} | ${'processing'} | ${true}
-    ${undefined}    | ${undefined} | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${true}
-    ${undefined}    | ${'#id4'}    | ${undefined} | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${true}
-    ${undefined}    | ${''}        | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${false}
-    ${undefined}    | ${'#id5'}    | ${aDate}     | ${aDate}       | ${''}          | ${'test'} | ${'processing'} | ${false}
-    ${{ type: '' }} | ${'#id6'}    | ${undefined} | ${aDate}       | ${undefined}   | ${'test'} | ${'processing'} | ${false}
-    ${undefined}    | ${'#id7'}    | ${aBadDate}  | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${false}
-    ${undefined}    | ${'#id8'}    | ${aDate}     | ${aBadDate}    | ${'test-task'} | ${'test'} | ${'processing'} | ${false}
-    ${undefined}    | ${'#id9'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${''}     | ${'processing'} | ${false}
-    ${undefined}    | ${'#id10'}   | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${''}           | ${false}
+    initialTask     | id           | creationDate | lastUpdateDate | type           | initiator | status          | progress                                   | expectedStatus
+    ${undefined}    | ${'#id1'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${undefined}                               | ${true}
+    ${undefined}    | ${'#id1'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${{ max: 1234567, min: 0, current: 5678 }} | ${true}
+    ${undefined}    | ${'#id2'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${undefined}    | ${undefined}                               | ${true}
+    ${undefined}    | ${'#id3'}    | ${aDate}     | ${undefined}   | ${'test-task'} | ${'test'} | ${'processing'} | ${undefined}                               | ${true}
+    ${undefined}    | ${undefined} | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${undefined}                               | ${true}
+    ${undefined}    | ${'#id4'}    | ${undefined} | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${undefined}                               | ${true}
+    ${undefined}    | ${''}        | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${undefined}                               | ${false}
+    ${undefined}    | ${'#id5'}    | ${aDate}     | ${aDate}       | ${''}          | ${'test'} | ${'processing'} | ${undefined}                               | ${false}
+    ${{ type: '' }} | ${'#id6'}    | ${undefined} | ${aDate}       | ${undefined}   | ${'test'} | ${'processing'} | ${undefined}                               | ${false}
+    ${undefined}    | ${'#id7'}    | ${aBadDate}  | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${undefined}                               | ${false}
+    ${undefined}    | ${'#id8'}    | ${aDate}     | ${aBadDate}    | ${'test-task'} | ${'test'} | ${'processing'} | ${undefined}                               | ${false}
+    ${undefined}    | ${'#id9'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${''}     | ${'processing'} | ${undefined}                               | ${false}
+    ${undefined}    | ${'#id10'}   | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${''}           | ${undefined}                               | ${false}
+    ${undefined}    | ${'#id1'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${{ max: 123, min: 0, current: 1234 }}     | ${false}
+    ${undefined}    | ${'#id1'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${{ max: 8, min: 10, current: 7 }}         | ${false}
+    ${undefined}    | ${'#id1'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${{ max: 100, min: 100, current: 7 }}      | ${false}
+    ${undefined}    | ${'#id1'}    | ${aDate}     | ${aDate}       | ${'test-task'} | ${'test'} | ${'processing'} | ${{ max: 100, min: 0, current: -10 }}      | ${false}
   `(
     'Given that id is <$id>, creationDate is <$creationDate>, lastUpdateDate is <$lastUpdateDate>, type is <$type>, initiator is <$initiator> and status is <$status>',
     ({
@@ -52,6 +58,7 @@ describe('Build a task', () => {
       type,
       initiator,
       status,
+      progress,
       expectedStatus
     }: Data) => {
       describe('When building a task', () => {
@@ -77,6 +84,9 @@ describe('Build a task', () => {
           if (status !== undefined) {
             taskBuilder = taskBuilder.withStatus(status)
           }
+          if (progress !== undefined) {
+            taskBuilder = taskBuilder.withProgress(progress)
+          }
           return taskBuilder.build()
         }
 
@@ -88,7 +98,8 @@ describe('Build a task', () => {
               lastUpdateDate: lastUpdateDate === undefined ? fakedDate : lastUpdateDate,
               type,
               initiator,
-              status: status === undefined ? baseStatus : status
+              status: status === undefined ? baseStatus : status,
+              progress
             })
           } else {
             expect(task).toThrowError(UnspecifiedError)
