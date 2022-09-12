909e6dcc3957bdf71cedd54bf4474996600aa69f
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-12 16:55:42 +0200

    test(core): refactor registerTask spec to handle progress

--- src/domain/task/usecase/register-task/registerTask.spec.ts
+++ src/domain/task/usecase/register-task/registerTask.spec.ts
@@ -13,6 +13,9 @@ import { getExpectedEventParameter } from '../../helper/test.helper'
 import { TaskStoreBuilder } from 'domain/task/store/builder/store.builder'
 import { RegisterTask } from 'domain/task/command/registerTask'
 import { TaskRegisteredPayload } from 'domain/task/event/taskRegistered'
+import { Progress } from 'domain/task/command/type'
+import { TaskProgressBoundsConfiguredPayload } from 'domain/task/event/taskProgressBoundsConfigured'
+import { TaskProgressValueSetPayload } from 'domain/task/event/taskProgressValueSet'
 
 type InitialProps = Readonly<{
   store: ReduxStore
@@ -66,7 +69,10 @@ describe('Register a task', () => {
     id: 'id1',
     timestamp: aDate,
     type: 'test#register-task',
-    initiator
+    initiator,
+    progress: {
+      min: 0
+    }
   }
 
   const taskToRegister2: RegisterTask = {
@@ -81,6 +87,40 @@ describe('Register a task', () => {
     timestamp: aDate,
     type: 'test#register-task',
     initiator
+  }
+
+  const taskToRegister4: RegisterTask = {
+    id: 'id4',
+    timestamp: aDate,
+    type: 'test#register-task',
+    initiator,
+    progress: {
+      min: 0,
+      max: 100,
+      current: 30
+    }
+  }
+
+  const taskToRegister5: RegisterTask = {
+    id: 'id5',
+    timestamp: aDate,
+    type: 'test#register-task',
+    initiator,
+    progress: {
+      min: 100,
+      max: 10
+    }
+  }
+
+  const taskToRegister6: RegisterTask = {
+    id: 'id5',
+    timestamp: aDate,
+    type: 'test#register-task',
+    initiator,
+    progress: {
+      min: 100,
+      current: 90
+    }
   }
 
   // Event payloads
@@ -100,6 +140,32 @@ describe('Register a task', () => {
     initiator
   }
 
+  const taskRegisteredPayload3: TaskRegisteredPayload = {
+    id: taskToRegister4.id,
+    timestamp: taskToRegister4.timestamp as Date,
+    type: taskToRegister4.type,
+    status: 'processing',
+    initiator
+  }
+
+  const taskProgressBoundsConfiguredPayload1: TaskProgressBoundsConfiguredPayload = {
+    id: taskToRegister1.id,
+    timestamp: taskToRegister1.timestamp as Date,
+    progressBounds: { max: taskToRegister1.progress?.max, min: taskToRegister1.progress?.min }
+  }
+
+  const taskProgressBoundsConfiguredPayload2: TaskProgressBoundsConfiguredPayload = {
+    id: taskToRegister4.id,
+    timestamp: taskToRegister4.timestamp as Date,
+    progressBounds: { max: taskToRegister4.progress?.max, min: taskToRegister4.progress?.min }
+  }
+
+  const taskProgressValueSetPayload1: TaskProgressValueSetPayload = {
+    id: taskToRegister4.id,
+    timestamp: taskToRegister4.timestamp as Date,
+    progressValue: taskToRegister4.progress?.current as number
+  }
+
   // Entities
   const task1 = new TaskBuilder()
     .withId(taskToRegister1.id)
@@ -108,6 +174,7 @@ describe('Register a task', () => {
     .withType(taskToRegister1.type)
     .withStatus('processing')
     .withInitiator(initiator)
+    .withProgress({ min: 0 })
     .build()
 
   const task2 = new TaskBuilder()
@@ -128,6 +195,16 @@ describe('Register a task', () => {
     .withInitiator(initiator)
     .build()
 
+  const task4 = new TaskBuilder()
+    .withId(taskToRegister4.id)
+    .withCreationDate(taskToRegister4.timestamp as Date)
+    .withLastUpdateDate(taskToRegister4.timestamp as Date)
+    .withType(taskToRegister4.type)
+    .withStatus('processing')
+    .withInitiator(initiator)
+    .withProgress(taskToRegister4.progress as Progress)
+    .build()
+
   const error = new ErrorBuilder()
     .withId(fakedUuid)
     .withTimestamp(fakedDate)
@@ -142,9 +219,12 @@ describe('Register a task', () => {
   describe.each`
     tasksToRegister                                        | expectedState                                 | expectedEventParameters
     ${[]}                                                  | ${getExpectedState([])}                       | ${[]}
-    ${[taskToRegister1]}                                   | ${getExpectedState([task1])}                  | ${[getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload1, initiator, fakedDate)]}
-    ${[taskToRegister1, taskToRegister2]}                  | ${getExpectedState([task1, task2])}           | ${[getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload2, initiator, fakedDate)]}
-    ${[taskToRegister1, taskToRegister2, taskToRegister3]} | ${getExpectedState([task1, task2, task3], 2)} | ${[getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload2, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', error, initiator, fakedDate)]}
+    ${[taskToRegister1]}                                   | ${getExpectedState([task1])}                  | ${[getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskProgressBoundsConfigured', taskProgressBoundsConfiguredPayload1, initiator, fakedDate)]}
+    ${[taskToRegister1, taskToRegister2]}                  | ${getExpectedState([task1, task2])}           | ${[getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskProgressBoundsConfigured', taskProgressBoundsConfiguredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload2, initiator, fakedDate)]}
+    ${[taskToRegister1, taskToRegister2, taskToRegister3]} | ${getExpectedState([task1, task2, task3], 2)} | ${[getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskProgressBoundsConfigured', taskProgressBoundsConfiguredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload2, initiator, fakedDate), getExpectedEventParameter('error/errorThrown', error, initiator, fakedDate)]}
+    ${[taskToRegister1, taskToRegister4]}                  | ${getExpectedState([task1, task4])}           | ${[getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskProgressBoundsConfigured', taskProgressBoundsConfiguredPayload1, initiator, fakedDate), getExpectedEventParameter('task/taskRegistered', taskRegisteredPayload3, initiator, fakedDate), getExpectedEventParameter('task/taskProgressBoundsConfigured', taskProgressBoundsConfiguredPayload2, initiator, fakedDate), getExpectedEventParameter('task/taskProgressValueSet', taskProgressValueSetPayload1, initiator, fakedDate)]}
+    ${[taskToRegister5]}                                   | ${getExpectedState([])}                       | ${[getExpectedEventParameter('error/errorThrown', error, initiator, fakedDate)]}
+    ${[taskToRegister6]}                                   | ${getExpectedState([])}                       | ${[getExpectedEventParameter('error/errorThrown', error, initiator, fakedDate)]}
   `(
     `Given that there are $tasksToRegister.length task(s) to register`,
     ({ tasksToRegister, expectedState, expectedEventParameters }: Data): void => {
