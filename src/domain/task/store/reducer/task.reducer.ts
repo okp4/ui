b4cb71f9124ed9cd75f31f0fcd8af088237444c1
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-12 16:48:09 +0200

    feat(redux): update reducer to handle progress property

--- src/domain/task/store/reducer/task.reducer.ts
+++ src/domain/task/store/reducer/task.reducer.ts
@@ -10,6 +10,11 @@ import type { TaskState } from '../appState'
 import type { TaskRegisteredPayload } from 'domain/task/event/taskRegistered'
 import type { TaskStatusAmendedPayload } from 'domain/task/event/taskStatusAmended'
 import type { TaskRemovedPayload } from 'domain/task/event/taskRemoved'
+import type { ConfigureTaskProgressBoundsActionTypes } from 'domain/task/usecase/configure-task-progress-bounds/actionCreators'
+import type { TaskProgressBoundsConfiguredPayload } from 'domain/task/event/taskProgressBoundsConfigured'
+import type { Progress } from 'domain/task/event/type'
+import type { SetTaskProgressValueActionTypes } from 'domain/task/usecase/set-task-progress-value/actionCreators'
+import type { TaskProgressValueSetPayload } from 'domain/task/event/taskProgressValueSet'
 
 const initialTaskState: TaskState<string, string> = {
   byId: OrderedMap<string, Task>(),
@@ -68,10 +73,57 @@ const removeTask = (
   }
 }
 
+const configureTaskProgressBounds = (
+  state: DeepReadonly<TaskState>,
+  payload: DeepReadonly<TaskProgressBoundsConfiguredPayload>
+): TaskState => {
+  const { id, timestamp, progressBounds }: TaskProgressBoundsConfiguredPayload = payload
+  const foundTaskById = state.byId.get(id)
+  const { max, min }: Progress = progressBounds
+  const bounds = {
+    ...(max !== undefined && { max }),
+    ...(min !== undefined && { min })
+  }
+  return {
+    ...state,
+    ...(foundTaskById && {
+      byId: state.byId.set(id, {
+        ...foundTaskById,
+        lastUpdateDate: timestamp,
+        progress: { ...foundTaskById.progress, ...bounds }
+      })
+    })
+  }
+}
+
+const setTaskProgressValue = (
+  state: DeepReadonly<TaskState>,
+  payload: DeepReadonly<TaskProgressValueSetPayload>
+): TaskState => {
+  const { id, timestamp, progressValue }: TaskProgressValueSetPayload = payload
+  const foundTaskById = state.byId.get(id)
+  return {
+    ...state,
+    ...(foundTaskById && {
+      byId: state.byId.set(id, {
+        ...foundTaskById,
+        lastUpdateDate: timestamp,
+        progress: { ...foundTaskById.progress, current: progressValue }
+      })
+    })
+  }
+}
+
 const task = (
   // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
   state: TaskState = initialTaskState,
-  action: DeepReadonly<RegisterTaskActionTypes | RemoveTaskActionTypes | AmendTaskStatusActionTypes>
+  action: DeepReadonly<
+    | RegisterTaskActionTypes
+    | RemoveTaskActionTypes
+    | AmendTaskStatusActionTypes
+    | ConfigureTaskProgressBoundsActionTypes
+    | SetTaskProgressValueActionTypes
+  >
 ): TaskState => {
   switch (action.type) {
     case 'task/taskRegistered':
@@ -82,6 +134,12 @@ const task = (
 
     case 'task/taskRemoved':
       return removeTask(state, action.payload)
+
+    case 'task/taskProgressBoundsConfigured':
+      return configureTaskProgressBounds(state, action.payload)
+
+    case 'task/taskProgressValueSet':
+      return setTaskProgressValue(state, action.payload)
 
     default:
       return state
@@ -95,6 +153,8 @@ const displayedTaskIds = (
     | RemoveTaskActionTypes
     | AmendTaskStatusActionTypes
     | AcknowledgeTaskActionTypes
+    | ConfigureTaskProgressBoundsActionTypes
+    | SetTaskProgressValueActionTypes
   >
 ): OrderedSet<string> => {
   switch (action.type) {
@@ -109,6 +169,12 @@ const displayedTaskIds = (
 
     case 'task/taskAcknowledged':
       return state.remove(action.payload.id)
+
+    case 'task/taskProgressBoundsConfigured':
+      return state.add(action.payload.id)
+
+    case 'task/taskProgressValueSet':
+      return state.add(action.payload.id)
 
     default:
       return state
