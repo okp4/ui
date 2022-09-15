bbad5278f27e5f47eff6b1c6ae3064526c75d714
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-15 17:03:49 +0200

    refactor(core): improve removeFile use case

--- src/domain/file/usecase/remove-file/removeFile.ts
+++ src/domain/file/usecase/remove-file/removeFile.ts
@@ -1,9 +1,31 @@
-import type { ThunkResult } from 'domain/file/store/store'
-import { RemoveFileActions } from './actionCreators'
+import { ThrowErrorActions } from 'domain/common/actionCreators'
+import { UnspecifiedError } from 'domain/file/entity/error'
+import { ErrorMapper } from 'domain/error/mapper/error.mapper'
+import type { RemoveFile } from 'domain/file/command/removeFile'
+import type { ReduxStore, ThunkResult } from 'domain/file/store/store'
+import type { DeepReadonly } from 'superTypes'
+import { RemoveFileActions } from '../../events/file-removed/actionCreators'
+
+const dispatchError = (error: unknown, dispatch: ReduxStore['dispatch']): void => {
+  const errorToDispatch = ErrorMapper.mapRawErrorToEntity(error)
+  dispatch(ThrowErrorActions.errorThrown(errorToDispatch))
+}
 
 export const removeFile =
-  (id: string): ThunkResult<Promise<void>> =>
+  (removeFilePayload: DeepReadonly<RemoveFile>): ThunkResult<Promise<void>> =>
   // eslint-disable-next-line @typescript-eslint/typedef
-  async dispatch => {
-    dispatch(RemoveFileActions.fileRemoved(id))
+  async (dispatch, getState) => {
+    const { id }: RemoveFile = removeFilePayload
+
+    if (!getState().file.byId.has(id)) {
+      dispatchError(
+        new UnspecifiedError(
+          `The provided id '${id}' does not exist... So we can't remove this file...`
+        ),
+        dispatch
+      )
+      return
+    }
+
+    dispatch(RemoveFileActions.fileRemoved({ id }))
   }
