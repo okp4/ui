527c35026c657eb6e78c0a53325bbaf704075df6
ccamel <camel.christophe@gmail.com>
2022-09-20 08:55:08 +0200

    feat: make the install camera system configurable

--- src/ui/molecules/networkGraph/ecs/system/installCameraSystem.ts
+++ src/ui/molecules/networkGraph/ecs/system/installCameraSystem.ts
@@ -4,20 +4,37 @@ import { ArcRotateCamera, Vector3 } from '@babylonjs/core'
 import { useInit } from '@javelin/ecs'
 import type { SystemFactory, World, WorldTickData } from '../type'
 
-export const InstallCameraSystem: SystemFactory = () => (world: World) => {
-  if (useInit()) {
-    const { scene }: WorldTickData = world.latestTickData
-    const camera = new ArcRotateCamera(
-      'camera-world',
-      Math.PI / 2,
-      Math.PI / 3,
-      300,
-      Vector3.Zero(),
-      scene
-    )
-    const canvas = scene.getEngine().getRenderingCanvas()
-
-    camera.setTarget(Vector3.Zero())
-    camera.attachControl(canvas, true)
-  }
+export type InstallCameraSystemOptions = {
+  name: string
+  alpha: number
+  beta: number
+  radius: number
+  target: Vector3
 }
+
+export const defaultArcRorateCamera = {
+  name: 'camera-world',
+  alpha: Math.PI / 2,
+  beta: Math.PI / 3,
+  radius: 300,
+  target: Vector3.Zero()
+}
+
+export const InstallCameraSystem: SystemFactory<InstallCameraSystemOptions> =
+  (options?: InstallCameraSystemOptions) => (world: World) => {
+    if (useInit()) {
+      const { scene }: WorldTickData = world.latestTickData
+      const camera = new ArcRotateCamera(
+        options?.name ?? defaultArcRorateCamera.name,
+        options?.alpha ?? defaultArcRorateCamera.alpha,
+        options?.beta ?? defaultArcRorateCamera.beta,
+        options?.radius ?? defaultArcRorateCamera.radius,
+        options?.target ?? defaultArcRorateCamera.target,
+        scene
+      )
+      const canvas = scene.getEngine().getRenderingCanvas()
+
+      camera.setTarget(Vector3.Zero())
+      camera.attachControl(canvas, true)
+    }
+  }
