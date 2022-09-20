4220f5f68af24aef2bfdb52c862f44a3b5456081
RaphaelEscrig <raphael.escrig@hotmail.fr>
2022-09-20 16:55:08 +0200

    refactor: listitem component
    refactor: change typeprops and code
    
    refactor: refactor listitem component

--- src/ui/atoms/listItem/ListItem.tsx
+++ src/ui/atoms/listItem/ListItem.tsx
@@ -4,9 +4,8 @@ import { Typography } from '../typography/Typography'
 import './listItem.scss'
 import { isString } from 'utils'
 import classNames from 'classnames'
-import type { ListProps } from '../list/List'
 
-export type ListItemProps = Pick<ListProps, 'layout'> & {
+export type ListItemProps = {
   /**
    * The main title of the item.
    */
@@ -16,78 +15,68 @@ export type ListItemProps = Pick<ListProps, 'layout'> & {
    */
   readonly description?: string | JSX.Element
   /**
-   * An element that can provide visual information or allow interactions.
-   * If layout is in list mode, it will be displayed on the left side.
-   * If layout is in grid mode, it will be displayed on the top right corner.
+   * An element that allow for further customization in the beginning of the reading flow.
+   * Is positioned to the left in a list layout and to the top right in a grid layout.
    */
   readonly firstElement?: JSX.Element
   /**
-   * An element that can provide visual information or allow interactions.
-   * If layout is in list mode, it will be displayed on the right side.
-   * If layout is in grid mode, it will be displayed at the bottom.
+   * An element that allow for further customization in the end of the reading flow.
+   * Is positioned to the right in a list layout and on the bottom in a grid layout.
    */
   readonly lastElement?: JSX.Element
   /**
+   * The layout style of ListItem.
+   */
+  readonly layout?: 'grid' | 'list'
+  /**
    * The callback function called when the list item is clicked.
-   * The `onClick` function is an event handler attached to the list item.
    */
   readonly onClick?: () => void
 }
 
-type TitleProps = Pick<ListItemProps, 'title'>
-type DescriptionProps = Pick<ListItemProps, 'description'>
+type TitleProps = Required<Pick<ListItemProps, 'title'>>
+type DescriptionProps = Required<Pick<ListItemProps, 'description'>>
 
-const Title: React.FC<TitleProps> = ({ title }: DeepReadonly<TitleProps>) => {
-  if (!title) return null
-
-  const titleTypography = (
-    <div className="okp4-listitem-title">
+const Title = ({ title }: DeepReadonly<TitleProps>): JSX.Element => (
+  <div className="okp4-listitem-title">
+    {isString(title) ? (
       <Typography color="inverted-text" fontSize="small" fontWeight="bold">
         {title}
       </Typography>
-    </div>
-  )
+    ) : (
+      title
+    )}
+  </div>
+)
 
-  return isString(title) ? titleTypography : <div className="okp4-listitem-title">{title}</div>
-}
-
-const Description: React.FC<DescriptionProps> = ({
-  description
-}: DeepReadonly<DescriptionProps>) => {
-  if (!description) return null
-
-  const descriptionTypography = (
-    <div className="okp4-listitem-description">
+const Description = ({ description }: DeepReadonly<DescriptionProps>): JSX.Element => (
+  <div className="okp4-listitem-description">
+    {isString(description) ? (
       <Typography color="inverted-text" fontSize="small">
         {description}
       </Typography>
-    </div>
-  )
+    ) : (
+      description
+    )}
+  </div>
+)
 
-  return isString(description) ? (
-    descriptionTypography
-  ) : (
-    <div className="okp4-listitem-description">{description}</div>
-  )
-}
-export const ListItem: React.FC<ListItemProps> = ({
+export const ListItem = ({
   title,
   description,
   firstElement,
   lastElement,
   onClick,
-  layout
+  layout = 'list'
 }: DeepReadonly<ListItemProps>): JSX.Element => {
-  const className = classNames('okp4-listitem-main', {
-    'layout-list': layout === 'list',
-    'layout-grid': layout === 'grid',
+  const className = classNames(`okp4-listitem-main layout-${layout}`, {
     clickable: !!onClick
   })
   return (
     <div className={className} onClick={onClick}>
       {firstElement && <div className="okp4-listitem-first-element">{firstElement}</div>}
-      <Title title={title} />
-      <Description description={description} />
+      {title && <Title title={title} />}
+      {description && <Description description={description} />}
       {lastElement && <div className="okp4-listitem-last-element">{lastElement}</div>}
     </div>
   )
