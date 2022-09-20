5764fb3075e800150fa48261015cf554cf1758c9
ccamel <camel.christophe@gmail.com>
2022-09-20 09:07:59 +0200

    feat: make prepare scene system configurable (a bit)

--- src/ui/molecules/networkGraph/ecs/system/prepareSceneSystem.ts
+++ src/ui/molecules/networkGraph/ecs/system/prepareSceneSystem.ts
@@ -4,14 +4,25 @@ import { Color3, Color4 } from '@babylonjs/core'
 import { useInit } from '@javelin/ecs'
 import type { SystemFactory, World, WorldTickData } from '../type'
 
+export type PrepareSceneSystemOptions = {
+  clearColor: Color4
+  ambientColor: Color3
+}
+
+export const defaultSceneParameters = {
+  clearColor: Color4.FromColor3(Color3.Black()),
+  ambientColor: Color3.FromHexString('#bbbbbb')
+}
+
 /**
  * The PrepareSceneSystem just initialize the scene with some parameters.
  */
-export const PrepareSceneSystem: SystemFactory = () => (world: World) => {
-  if (useInit()) {
-    const { scene }: WorldTickData = world.latestTickData
+export const PrepareSceneSystem: SystemFactory<PrepareSceneSystemOptions> =
+  (options?: PrepareSceneSystemOptions) => (world: World) => {
+    if (useInit()) {
+      const { scene }: WorldTickData = world.latestTickData
 
-    scene.clearColor = Color4.FromColor3(Color3.Black())
-    scene.ambientColor = Color3.FromHexString('#bbbbbb')
+      scene.clearColor = options?.clearColor ?? defaultSceneParameters.clearColor
+      scene.ambientColor = options?.ambientColor ?? defaultSceneParameters.ambientColor
+    }
   }
-}
