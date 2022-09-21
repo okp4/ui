0b2bdaa15783557578816cd511dd784d3605c74b
ErikssonJoakim <j.eriksson@okp4.com>
2022-09-21 19:37:11 +0200

    feat: parameterize node tooltip system

--- src/ui/molecules/networkGraph/ecs/system/nodeTooltipSystem.ts
+++ src/ui/molecules/networkGraph/ecs/system/nodeTooltipSystem.ts
@@ -1,16 +1,25 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 import type { ActionEvent, Mesh, Scene } from '@babylonjs/core'
 import { ActionManager, ExecuteCodeAction } from '@babylonjs/core'
 import type { ComponentOf } from '@javelin/ecs'
 import { createQuery, useInit, useMonitor, useRef } from '@javelin/ecs'
-import { LabelComponent, MeshComponent, NodeComponent } from '../schema'
+import { MeshComponent, NodeComponent } from '../schema'
 import type { SystemFactory, World, WorldTickData } from '../type'
+import type { Node } from 'ngraph.graph'
+import type { DeepReadonly } from 'superTypes'
 
 const queries = {
-  nodes: createQuery(NodeComponent, MeshComponent, LabelComponent)
+  nodes: createQuery(NodeComponent, MeshComponent)
 }
 
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+export type TooltipExtractor<T> = (node: DeepReadonly<Node<T>>) => HTMLDivElement | null
+
+export type NodeTooltipOptions<T> = {
+  tooltipExtractor: TooltipExtractor<T>
+}
+
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 const createTooltipDiv = (scene: Scene): HTMLDivElement | null => {
   const canvas = scene.getEngine().getRenderingCanvas()
   const document = canvas?.ownerDocument
@@ -33,57 +42,80 @@ const createTooltipDiv = (scene: Scene): HTMLDivElement | null => {
   return null
 }
 
-/**
- * The NodeTooltipSystem is in charge of displaying a tooltip (in html) during the hover of an entity with the components
- * label, mesh and node.
- */
-// eslint-disable-next-line max-lines-per-function
-export const NodeTooltipSystem: SystemFactory = () => (world: World) => {
-  const { scene }: WorldTickData = world.latestTickData
-  const tooltipDivRef = useRef<HTMLDivElement | null>(null)
+export const defaultTooltipExtractor: TooltipExtractor<Record<string, string> | null> = (
+  node: DeepReadonly<Node<Record<string, string> | null>>
+) => {
+  const label = node.data?.['label'] ?? ''
 
-  if (useInit()) {
-    tooltipDivRef.value = createTooltipDiv(scene)
+  if (label) {
+    const div = document.createElement('div')
+    div.innerText = node.data?.['label'] ?? ''
+
+    return div
   }
 
-  useMonitor(
-    queries.nodes,
-    (
-      _: number,
-      [, m, l]: [
-        ComponentOf<typeof NodeComponent>,
-        ComponentOf<typeof MeshComponent>,
-        ComponentOf<typeof LabelComponent>
-      ]
-    ) => {
-      const mesh = m as Mesh
-      const actionManager = mesh.actionManager ? mesh.actionManager : new ActionManager(scene)
-
-      const actions = [
-        new ExecuteCodeAction(ActionManager.OnPointerOverTrigger, (evt: ActionEvent) => {
-          if (tooltipDivRef.value) {
-            const width = scene.getEngine().getRenderingCanvas()?.width ?? 1
-            const height = scene.getEngine().getRenderingCanvas()?.height ?? 0
-            const [x, y]: [number, number] = [evt.pointerX, evt.pointerY]
-
-            tooltipDivRef.value.style.left = `${x}px`
-            tooltipDivRef.value.style.top = `${y}px`
-            tooltipDivRef.value.style.color = 'white'
-            tooltipDivRef.value.style.transform = `translate(-${(x / width) * 100}%, ${
-              height - y < 100 ? 'calc(-100% - 8px)' : '21px'
-            })`
-            tooltipDivRef.value.innerText = l.label
-          }
-        }),
-        new ExecuteCodeAction(ActionManager.OnPointerOutTrigger, () => {
-          if (tooltipDivRef.value) {
-            tooltipDivRef.value.innerText = ''
-          }
-        })
-      ]
-      actions.forEach(actionManager.registerAction.bind(actionManager))
-
-      mesh.actionManager = actionManager
-    }
-  )
+  return null
 }
+
+/**
+ * The NodeTooltipSystem is in charge of displaying a tooltip (in html) during the hover of an entity with the components
+ * mesh and node.
+ */
+export const NodeTooltipSystem: SystemFactory<NodeTooltipOptions<World>> =
+  // eslint-disable-next-line max-lines-per-function, @typescript-eslint/prefer-readonly-parameter-types
+  (options?: DeepReadonly<NodeTooltipOptions<World>>) => (world: World) => {
+    const { scene, graph }: WorldTickData = world.latestTickData
+    const tooltipDivRef = useRef<HTMLDivElement | null>(null)
+
+    if (useInit()) {
+      tooltipDivRef.value = createTooltipDiv(scene)
+    }
+
+    useMonitor(
+      queries.nodes,
+      (
+        _: number,
+        [n, m]: DeepReadonly<[ComponentOf<typeof NodeComponent>, ComponentOf<typeof MeshComponent>]>
+      ) => {
+        const mesh = m as Mesh
+        const actionManager = mesh.actionManager ? mesh.actionManager : new ActionManager(scene)
+        const tooltipExtractor = options?.tooltipExtractor ?? defaultTooltipExtractor
+        const actions = [
+          // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+          new ExecuteCodeAction(ActionManager.OnPointerOverTrigger, (evt: ActionEvent) => {
+            if (tooltipDivRef.value) {
+              const node = graph.getNode(n.id)
+
+              const width = scene.getEngine().getRenderingCanvas()?.width ?? 1
+              const height = scene.getEngine().getRenderingCanvas()?.height ?? 0
+              const [x, y]: [number, number] = [evt.pointerX, evt.pointerY]
+
+              tooltipDivRef.value.style.left = `${x}px`
+              tooltipDivRef.value.style.top = `${y}px`
+              tooltipDivRef.value.style.color = 'white'
+              tooltipDivRef.value.style.transform = `translate(-${(x / width) * 100}%, ${
+                height - y < 100 ? 'calc(-100% - 8px)' : '21px'
+              })`
+
+              if (node) {
+                const tooltip = tooltipExtractor(node)
+                tooltipDivRef.value.innerText = ''
+
+                if (tooltip) {
+                  tooltipDivRef.value.appendChild(tooltip)
+                }
+              }
+            }
+          }),
+          new ExecuteCodeAction(ActionManager.OnPointerOutTrigger, () => {
+            if (tooltipDivRef.value) {
+              tooltipDivRef.value.innerText = ''
+            }
+          })
+        ]
+        actions.forEach(actionManager.registerAction.bind(actionManager))
+
+        mesh.actionManager = actionManager
+      }
+    )
+  }
