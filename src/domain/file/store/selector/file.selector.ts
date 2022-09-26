e82e38ce121cfb2b116d51e9c31c60431ccd73c3
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-26 14:34:12 +0200

    refactor(redux): update file selectors

--- src/domain/file/store/selector/file.selector.ts
+++ src/domain/file/store/selector/file.selector.ts
@@ -19,3 +19,28 @@ export const getFiles: (state: DeepReadonly<AppState>) => FileDescriptor[] = cre
       .toArray()
       .map(({ id, name, size, type }: FileEntity) => ({ id, name, size, type }))
 )
+
+export const getFilesSizeByIds: (state: DeepReadonly<AppState>, ids: Readonly<string[]>) => number =
+  createSelector(
+    [
+      (state: DeepReadonly<AppState>): FileById => state.file.byId,
+      (_state: DeepReadonly<AppState>, ids: Readonly<string[]>): Readonly<string[]> => ids
+    ],
+    (files: DeepReadonly<FileById>, ids: Readonly<string[]>): number =>
+      ids.reduce((acc: number, cur: string) => {
+        const found = files.get(cur)
+        return found ? acc + found.size : acc
+      }, 0)
+  )
+
+export const getTotalFilesSize: (state: DeepReadonly<AppState>) => number = createSelector(
+  (state: DeepReadonly<AppState>): AppState => state,
+  (state: DeepReadonly<AppState>): number =>
+    getFilesSizeByIds(
+      state,
+      state.file.byId
+        .toIndexedSeq()
+        .toArray()
+        .map((value: FileEntity) => value.id)
+    )
+)
