5b4d2895f80e0e99b405ca066ab2d662009c4929
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-26 14:24:03 +0200

    test(redux): add dependencies to store builder test suite

--- src/domain/file/store/builder/store.builder.spec.ts
+++ src/domain/file/store/builder/store.builder.spec.ts
@@ -7,10 +7,13 @@ import type { FileStoreParameters } from './store.builder'
 import type { AppState } from '../appState'
 import { FileBuilder } from 'domain/file/builder/file.builder'
 import { FileEntity } from 'domain/file/entity/file'
+import { Dependencies } from '../store'
+import { FileRegistryGateway } from 'adapters/file'
 
 type Data = Readonly<
   Partial<{
     initialStoreParameters: FileStoreParameters
+    dependencies: Dependencies
     eventBus: EventBus
     preloadedState: AppState
   }> & {
@@ -18,6 +21,7 @@ type Data = Readonly<
   }
 >
 const eventBusInstance = new EventBus()
+const fileRegistryGateway = new FileRegistryGateway()
 const file1 = new FileBuilder()
   .withId('id1')
   .withName('image1')
@@ -35,23 +39,28 @@ const state1: AppState = {
 
 describe('Build a File store', () => {
   describe.each`
-    initialStoreParameters            | eventBus            | preloadedState | expectedStatus
-    ${undefined}                      | ${eventBusInstance} | ${undefined}   | ${true}
-    ${undefined}                      | ${eventBusInstance} | ${state1}      | ${true}
-    ${{ eventBus: eventBusInstance }} | ${undefined}        | ${undefined}   | ${true}
-    ${undefined}                      | ${eventBusInstance} | ${{}}          | ${false}
-    ${undefined}                      | ${{}}               | ${undefined}   | ${false}
-    ${undefined}                      | ${undefined}        | ${undefined}   | ${false}
-    ${{ preloadedState: {} }}         | ${undefined}        | ${undefined}   | ${false}
-    ${{ eventBus: {} }}               | ${undefined}        | ${undefined}   | ${false}
+    initialStoreParameters                            | dependencies                    | eventBus            | preloadedState | expectedStatus
+    ${undefined}                                      | ${undefined}                    | ${eventBusInstance} | ${undefined}   | ${true}
+    ${undefined}                                      | ${{ fileRegistryGateway }}      | ${eventBusInstance} | ${undefined}   | ${true}
+    ${undefined}                                      | ${undefined}                    | ${eventBusInstance} | ${state1}      | ${true}
+    ${{ eventBus: eventBusInstance }}                 | ${undefined}                    | ${undefined}        | ${undefined}   | ${true}
+    ${undefined}                                      | ${undefined}                    | ${eventBusInstance} | ${{}}          | ${false}
+    ${undefined}                                      | ${undefined}                    | ${{}}               | ${undefined}   | ${false}
+    ${undefined}                                      | ${undefined}                    | ${undefined}        | ${undefined}   | ${false}
+    ${{ preloadedState: {} }}                         | ${undefined}                    | ${undefined}        | ${undefined}   | ${false}
+    ${{ eventBus: {} }}                               | ${undefined}                    | ${undefined}        | ${undefined}   | ${false}
+    ${undefined}                                      | ${{ foo: fileRegistryGateway }} | ${eventBusInstance} | ${undefined}   | ${false}
+    ${{ dependencies: { foo: fileRegistryGateway } }} | ${undefined}                    | ${eventBusInstance} | ${undefined}   | ${false}
   `(
-    'Given that eventBus is <$eventBus> adn preloadedState is <$preloadedState>',
-    ({ initialStoreParameters, eventBus, preloadedState, expectedStatus }: Data) => {
+    'Given that dependencies are <$dependencies>, eventBus is <$eventBus> adn preloadedState is <$preloadedState>',
+    ({ initialStoreParameters, dependencies, eventBus, preloadedState, expectedStatus }: Data) => {
       describe('When building a File Store', () => {
         const store = (): Store<AppState, AnyAction> => {
           // eslint-disable-next-line functional/no-let
           let fileStoreBuilder = new FileStoreBuilder(initialStoreParameters)
-
+          if (dependencies !== undefined) {
+            fileStoreBuilder = fileStoreBuilder.withDependencies(dependencies)
+          }
           if (eventBus !== undefined) {
             fileStoreBuilder = fileStoreBuilder.withEventBus(eventBus)
           }
