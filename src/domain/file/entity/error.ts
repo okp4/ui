2ab3447508279379ebf8f36c111f5e3939ff6278
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-09-26 14:20:33 +0200

    feat(core): enhance error entity

--- src/domain/file/entity/error.ts
+++ src/domain/file/entity/error.ts
@@ -1,6 +1,24 @@
+import type { DeepReadonly } from 'superTypes'
+
 export class UnspecifiedError extends Error {
   constructor(message?: string) {
     super(message)
     this.name = 'UnspecifiedError'
+  }
+}
+
+export class GatewayError extends Error {
+  constructor(message?: string) {
+    super(message)
+    this.name = 'GatewayError'
+  }
+}
+
+export class UploadError extends Error {
+  public context: Record<string, string> | undefined = {}
+  constructor(message?: string, context?: DeepReadonly<Record<string, string>>) {
+    super(message)
+    this.name = 'UploadError'
+    this.context = context
   }
 }
