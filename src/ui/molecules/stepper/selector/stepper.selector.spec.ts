a090999b46902f5f778fd9bdac2ea5510afdd4d9
Joey <joey.remion@gmail.com>
2022-09-30 17:52:06 +0200

    refactor(stepper): remove useless selectors

--- src/ui/molecules/stepper/selector/stepper.selector.spec.ts
+++ src/ui/molecules/stepper/selector/stepper.selector.spec.ts
@@ -2,59 +2,7 @@ import { StepperState } from 'hook/useStepper'
 import { List as ImmutableList, OrderedMap } from 'immutable'
 import { StepId, StepStatus } from 'ui/index'
 import { Step } from '../Stepper'
-import { getCurrentStep, getStepStatusByStepId, getUpdatedSteps } from './stepper.selector'
-
-describe('Considering the getStepStatusByStepId function', () => {
-  const emptyState: StepperState = {
-    currentStepId: '',
-    stepsStatus: OrderedMap(),
-    enabledSteps: ImmutableList()
-  }
-
-  const state: StepperState = {
-    currentStepId: 'step3',
-    stepsStatus: OrderedMap<StepId, StepStatus>()
-      .set('step1', 'completed')
-      .set('step2', 'disabled')
-      .set('step3', 'invalid')
-      .set('step4', 'uncompleted')
-      .set('step5', 'disabled')
-      .set('step6', 'uncompleted'),
-    enabledSteps: ImmutableList(['step1', 'step3', 'step4', 'step6'])
-  }
-
-  describe.each`
-    stepId       | state         | expectedStatus
-    ${undefined} | ${emptyState} | ${'uncompleted'}
-    ${undefined} | ${state}      | ${'uncompleted'}
-    ${''}        | ${state}      | ${'uncompleted'}
-    ${'step1'}   | ${state}      | ${'completed'}
-    ${'step2'}   | ${state}      | ${'disabled'}
-    ${'step3'}   | ${state}      | ${'invalid'}
-    ${'step4'}   | ${state}      | ${'uncompleted'}
-    ${'step5'}   | ${state}      | ${'disabled'}
-    ${'step6'}   | ${state}      | ${'uncompleted'}
-  `(
-    'Given a step id <$stepId> and a state',
-    ({
-      stepId,
-      state,
-      expectedStatus
-    }: {
-      stepId: StepId
-      state: StepperState
-      expectedStatus: OrderedMap<StepId, StepStatus>
-    }) => {
-      describe('When calling getStepStatusByStepId function', () => {
-        const result = getStepStatusByStepId(stepId, state)
-
-        test(`Then, expected status is ${JSON.stringify(expectedStatus)}`, () => {
-          expect(result).toStrictEqual(expectedStatus)
-        })
-      })
-    }
-  )
-})
+import { getUpdatedSteps } from './stepper.selector'
 
 describe('Considering the getUpdatedSteps function', () => {
   const steps: Step[] = [
@@ -140,80 +88,6 @@ describe('Considering the getUpdatedSteps function', () => {
 
         test(`Then, updated steps are ${expectedSteps}`, () => {
           expect(result).toStrictEqual(expectedSteps)
-        })
-      })
-    }
-  )
-})
-
-describe('Considering the getCurrentStep function', () => {
-  const step1: Step = {
-    id: 'step1',
-    status: 'completed'
-  }
-  const step2: Step = {
-    id: 'step2',
-    status: 'disabled'
-  }
-  const step3: Step = {
-    id: 'step3',
-    status: 'invalid'
-  }
-  const step4: Step = {
-    id: 'step4'
-  }
-  const step5: Step = {
-    id: 'step5',
-    status: 'disabled'
-  }
-  const step6: Step = {
-    id: 'step6'
-  }
-  const steps: Step[] = [step1, step2, step3, step4, step5, step6]
-
-  const state1: StepperState = {
-    currentStepId: 'step3',
-    stepsStatus: OrderedMap<StepId, StepStatus>()
-      .set('step1', 'completed')
-      .set('step2', 'disabled')
-      .set('step3', 'completed')
-      .set('step4', 'uncompleted')
-      .set('step5', 'disabled')
-      .set('step6', 'uncompleted'),
-    enabledSteps: ImmutableList(['step1', 'step3', 'step4', 'step6'])
-  }
-
-  const state2: StepperState = {
-    ...state1,
-    currentStepId: 'step4'
-  }
-
-  const state3: StepperState = {
-    ...state1,
-    currentStepId: 'step6'
-  }
-
-  describe.each`
-    steps    | state     | expectedStep
-    ${steps} | ${state1} | ${step3}
-    ${steps} | ${state2} | ${step4}
-    ${steps} | ${state3} | ${step6}
-  `(
-    'Given a steps array <$steps> and a state',
-    ({
-      steps,
-      state,
-      expectedStep
-    }: {
-      steps: Step[]
-      state: StepperState
-      expectedStep: Step
-    }) => {
-      describe('When calling getCurrentStep function', () => {
-        const result = getCurrentStep(steps, state)
-
-        test(`Then, the expected step is ${JSON.stringify(expectedStep)}`, () => {
-          expect(result).toStrictEqual(expectedStep)
         })
       })
     }
--- src/ui/molecules/stepper/selector/stepper.selector.ts
+++ src/ui/molecules/stepper/selector/stepper.selector.ts
@@ -1,17 +1,6 @@
 import type { StepperState } from 'hook/useStepper'
 import type { DeepReadonly } from 'superTypes'
-import type { Step, StepId, StepStatus } from 'ui/index'
-
-/**
- * Retrieve the status of the step by its id.
- * @param stepId The id of the step.
- * @param state The state of the Stepper
- * @returns The step status.
- */
-export const getStepStatusByStepId = (
-  stepId: StepId,
-  state: DeepReadonly<StepperState>
-): StepStatus => state.stepsStatus.get(stepId) ?? 'uncompleted'
+import type { Step } from 'ui/index'
 
 /**
  * Get the updated steps with the new status from the Stepper state.
@@ -22,19 +11,8 @@ export const getStepStatusByStepId = (
 export const getUpdatedSteps = (
   steps: DeepReadonly<Step[]>,
   state: DeepReadonly<StepperState>
-): DeepReadonly<Step[]> =>
+): Step[] =>
   steps.map((step: DeepReadonly<Step>) => ({
     ...step,
     status: state.stepsStatus.get(step.id)
   }))
-
-/**
- * Retrieve the the current step.
- * @param steps The steps array.
- * @param state The state of the Stepper.
- * @returns The current step.
- */
-export const getCurrentStep = (
-  steps: DeepReadonly<Step[]>,
-  state: DeepReadonly<StepperState>
-): Step | undefined => steps.find((step: DeepReadonly<Step>) => step.id === state.currentStepId)
