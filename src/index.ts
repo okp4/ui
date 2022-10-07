2010fe1de99cb223c266bef77d700dd74eaed316
Joey <joey.remion@gmail.com>
2022-10-07 14:53:37 +0200

    refactor: export every utils types and functions

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ export * from './ui/index'
 export * from './hook/index'
 export * from './context/index'
 export * from './i18n/utils'
-export type { DeepReadonly, UseState, UseReducer } from './superTypes'
-export type { Entity } from './domain/common/type'
 export * from './i18n/types'
+export type { Entity } from './domain/common/type'
 export * from './domain/error/index'
 export * from './domain/task/index'
 export * from './domain/faucet/index'
@@ -12,17 +11,5 @@ export * from './domain/wallet/index'
 export * from './domain/file/index'
 export * from './eventBus/index'
 export * from './adapters/index'
-export type { Truthy, SelectOption } from './utils'
-export {
-  truthy,
-  isString,
-  toPercent,
-  toReadableFileSize,
-  isFileTypeAccepted,
-  areFilesAccepted,
-  sortSelectOptionAsc,
-  sortSelectOptionDesc,
-  compareStrings,
-  asImmutable,
-  asMutable
-} from './utils'
+export * from './superTypes'
+export * from './utils'
