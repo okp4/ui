f107bbf4f2e773689121b37e61f2ade3d171d497
Frédéric Vilcot <fredericvilcot@gmail.com>
2022-12-05 17:50:31 +0100

    fix(adapter): switch cosmjs AccountData type to @keplr-wallet/types

--- src/adapters/wallet/secondary/mapper/account.mapper.ts
+++ src/adapters/wallet/secondary/mapper/account.mapper.ts
@@ -1,6 +1,6 @@
 import { AccountBuilder } from 'domain/wallet/builders/account.builder'
 import type { Account } from 'domain/wallet/entities/wallet'
-import type { AccountData } from '@cosmjs/launchpad'
+import type { AccountData } from '@keplr-wallet/types'
 import type { DeepReadonly } from 'superTypes'
 
 export class KeplrAccountMapper {
